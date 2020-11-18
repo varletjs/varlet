6effb177ade2f8e69a2756f9481829688c0f3961
BeADre <beadre_wang@163.com>
2020-11-18 19:52:08 +0800

    feat: add example of checkbox

---
+++ packages/varlet-ui/src/checkbox/example/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<input type="checkbox" />1 <input type="checkbox" />2
+		<input type="checkbox" />3
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Index',
+}
+</script>
+
+<style scoped>
+.button {
+	background: antiquewhite;
+}
+</style>
