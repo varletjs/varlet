3a683392451083c5ba7bdc72d2fde67f993382fe
kanghuiyi <1193185379@qq.com>
2020-12-30 18:19:50 +0800

    添加图标

--- packages/varlet-ui/src/chip/Chip.vue
+++ packages/varlet-ui/src/chip/Chip.vue
@@ -1,43 +1,55 @@
 <template>
-	<transition name="var-fade">
-		<span
-			v-ripple="{ disabled }"
-			class="var-chip var--box var-elevation--2"
-			:style="{
-				color: textcolor ? textcolor : hollow ? color : '#fff',
-				background: hollow ? '#fff' : color,
-				borderColor: hollow ? color : color ? color : 'none',
-			}"
-			:class="[
-				`var-chip--${type}`,
+  <transition name="var-fade">
+    <!--        :style="{-->
+    <!--        color: textcolor ? textcolor : hollow ? color : '',-->
+    <!--        background: hollow ? '#fff' : color,-->
+    <!--        borderColor: hollow ? color : color ? color : 'none',-->
+    <!--        }"-->
+    <span
+      v-ripple="{ disabled }"
+      class="var-chip var--box"
+      :class="[
 				`var-chip--${size}`,
 				block ? 'var--flex' : 'var--inline-flex',
 				round ? 'var-chip--round' : null,
-				hollow ? 'var-chip--hollow' : null,
+				plain ? `var-chip--plain-${type}` : `var-chip--${type}`,
+				plain ? `var-chip--plain` : `var-elevation--2`,
 			]"
-		>
-			<slot name="left"></slot>
-			<slot />
+      :style="{
+        color: textColor ? textColor : plain ? color : null,
+        background: plain ? '#fff' : color,
+        borderColor: plain ? color : color ? color : 'none',
+        }"
+    >
+        <slot name="left"></slot>
+			<slot/>
 			<slot name="right"></slot>
-			<span v-if="paralyse && !closeIcon" class="var-chip--close" @click="$props.onClose">x</span>
-			<span v-else-if="paralyse && closeIcon" class="var-chip--close" @click="$props.onClose">{{ closeIcon }}</span>
+			<span v-if="closable " class="var-chip--close" @click="$props.onClose">
+        <var-icon :name="`${iconName?iconName:'close-circle'}`"></var-icon>
+      </span>
 		</span>
-	</transition>
+  </transition>
 </template>
 
 <script lang="ts">
-import Ripple from '../ripple'
-import { defineComponent } from 'vue'
-import { props } from './props'
+	import Ripple from '../ripple'
+	import { defineComponent } from 'vue'
+	import { props } from './props'
+	import Icon from '../icon'
 
-export default defineComponent({
-	name: 'VarChip',
-	components: {},
-	directives: { Ripple },
-	props,
-})
+	export default defineComponent({
+		name: 'VarChip',
+		components: {
+			[Icon.name]: Icon
+		},
+		directives: { Ripple },
+		props,
+		setup(props) {
+			console.log(props)
+		}
+	})
 </script>
 
 <style lang="less">
-@import './chip';
+  @import './chip';
 </style>
--- packages/varlet-ui/src/chip/example/index.vue
+++ packages/varlet-ui/src/chip/example/index.vue
@@ -2,70 +2,77 @@
   <div class="example">
     <div>标签类型</div>
     <div class="example-inner">
-      <var-chip>default chip</var-chip>
-      <var-chip type="primary">primary</var-chip>
-      <var-chip type="info">info</var-chip>
-      <var-chip type="success">success</var-chip>
-      <var-chip type="warning">warning</var-chip>
-      <var-chip type="danger">danger</var-chip>
+      <var-chip>默认标签</var-chip>
+      <var-chip type="primary">主要标签</var-chip>
+      <var-chip type="info">信息标签</var-chip>
+      <var-chip type="success">成功标签</var-chip>
+      <var-chip type="warning">警告标签</var-chip>
+      <var-chip type="danger">危险标签</var-chip>
     </div>
     <div>标签大小</div>
     <div>
-      <var-chip type="primary" size="large">标签</var-chip>
-      <var-chip type="primary">标签</var-chip>
-      <var-chip type="primary" size="small">标签</var-chip>
-      <var-chip type="primary" size="mini">标签</var-chip>
+      <var-chip type="primary" size="large">大型标签</var-chip>
+      <var-chip type="primary">常规标签</var-chip>
+      <var-chip type="primary" size="small">小型标签</var-chip>
+      <var-chip type="primary" size="mini">迷你标签</var-chip>
     </div>
     <div>空心标签</div>
     <div>
-      <var-chip hollow>标签</var-chip>
-      <var-chip type="primary" hollow>标签</var-chip>
-      <var-chip type="info" hollow>标签</var-chip>
-      <var-chip type="success" hollow>标签</var-chip>
-      <var-chip type="warning" hollow>标签</var-chip>
-      <var-chip type="danger" hollow>标签</var-chip>
+      <var-chip plain>标签</var-chip>
+      <var-chip type="primary" plain>标签</var-chip>
+      <var-chip type="info" plain>标签</var-chip>
+      <var-chip type="success" plain>标签</var-chip>
+      <var-chip type="warning" plain>标签</var-chip>
+      <var-chip type="danger" plain>标签</var-chip>
     </div>
-    <div>圆角标签</div>
+    <div>不使用圆角标签</div>
     <div>
-      <var-chip round>标签</var-chip>
-      <var-chip type="primary" round>标签</var-chip>
-      <var-chip type="info" round>标签</var-chip>
-      <var-chip type="warning" round>标签</var-chip>
-      <var-chip type="success" round>标签</var-chip>
-      <var-chip type="danger" round>标签</var-chip>
+      <var-chip :round="false">标签</var-chip>
+      <var-chip type="primary" :round="false">标签</var-chip>
+      <var-chip type="info" :round="false">标签</var-chip>
+      <var-chip type="warning" :round="false">标签</var-chip>
+      <var-chip type="success" :round="false">标签</var-chip>
+      <var-chip type="danger" :round="false">标签</var-chip>
     </div>
 
     <div>可关闭标签</div>
     <div>
-      <var-chip type="primary" paralyse v-if="show" @close="show=false">标签</var-chip>
-      <var-chip type="primary" hollow paralyse v-if="show1" @close="show1=false">标签</var-chip>
-      <var-chip type="primary" paralyse v-if="show2" @close="show2=false" round>标签</var-chip>
-      <var-chip type="primary" paralyse v-if="show4" @close="show4=false" close-icon="=">标签</var-chip>
-      <var-chip type="primary" paralyse size="mini" v-if="show3" @close="show3=false">标签</var-chip>
+      <var-chip type="info" closable plain v-if="show" @close="show=false">标签</var-chip>
+      <var-chip type="primary" plain closable v-if="show1" @close="show1=false"  icon-name="delete">标签
+      </var-chip>
+      <var-chip type="primary" closable v-if="show2" @close="show2=false">标签</var-chip>
+      <var-chip type="info" closable v-if="show4" @close="show4=false" :round="false">标签</var-chip>
+      <var-chip type="success" closable size="mini" v-if="show3" @close="show3=false">标签</var-chip>
     </div>
 
     <div>自定义颜色标签</div>
     <div>
       <var-chip color="#7232dd">标签</var-chip>
-      <var-chip color="#7232dd" hollow>标签</var-chip>
-      <var-chip color="#faecd8" textcolor="#e6a23c" hollow>标签</var-chip>
-      <var-chip color="#faecd8" textcolor="#e6a23c">标签</var-chip>
-      <var-chip color="#faecd8" textcolor="#e6a23c" paralyse v-if="show6" @close="show6=false">标签</var-chip>
-      <var-chip color="#faecd8" textcolor="#e6a23c" hollow paralyse v-if="show5" @close="show5=false"> 标签</var-chip>
-
-
+      <var-chip color="#7232dd" plain>标签</var-chip>
+      <var-chip color="#faecd8" text-color="#e6a23c" plain>标签</var-chip>
+      <var-chip color="#faecd8" text-color="#e6a23c">标签</var-chip>
+      <var-chip color="#faecd8" text-color="#e6a23c" closable v-if="show6" @close="show6=false">标签</var-chip>
+      <var-chip color="#faecd8" text-color="#e6a23c" plain closable v-if="show5" @close="show5=false"> 标签</var-chip>
     </div>
     <div>添加插槽</div>
     <div>
-      <var-chip type="primary">
+      <var-chip type="primary" plain closable>
         <template #left>
-          <span>+</span>
+          <var-icon name="star"></var-icon>
         </template>
         标签
       </var-chip>
+
+      <var-chip type="info" plain closable icon-name="delete">
+        <template #left>
+          <var-icon name="fire"></var-icon>
+        </template>
+        标签
+      </var-chip>
+
       <var-chip type="primary">
         <template #right>
-          <span>-</span>
+          <var-icon name="cake-variant"></var-icon>
         </template>
         标签
       </var-chip>
@@ -77,11 +84,13 @@
 <script lang="ts">
 	import { defineComponent, Ref, ref } from 'vue'
 	import Chip from '..'
+	import Icon from '../../icon'
 
 	export default defineComponent({
 		name: 'ChipExample',
 		components: {
-			[Chip.name]: Chip
+			[Chip.name]: Chip,
+			[Icon.name]: Icon
 		},
 		setup() {
 			const show: Ref<boolean> = ref(true)
