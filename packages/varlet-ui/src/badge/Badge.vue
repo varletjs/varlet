cd082d1bf14ea2a9f2721cea772409ac4f3b3d82
kanghuiyi <1193185379@qq.com>
2021-01-07 16:49:20 +0800

    fix:solve the problem of label slot distance

--- packages/varlet-ui/src/badge/Badge.vue
+++ packages/varlet-ui/src/badge/Badge.vue
@@ -4,16 +4,17 @@
       <span
         v-show="!hidden"
         :class="[
-      `var-badge--${type}`,
-      'var-badge__content',
-      {'var-badge--dot':dot},
-      position?`var-badge--${position} var-badge--position`:null,
-      dotPosition
-      ]"
-        :style="{background:color}"
+          'var-badge__content',
+          `var-badge--${type}`,
+          position ? `var-badge--${position} var-badge--position` : null,
+          dotPosition,
+          { 'var-badge--dot' : dot },
+        ]"
+        :style="{background : color}"
+        v-bind="$attrs"
       >
         <var-icon v-if="icon" :name="icon"></var-icon>
-        <span v-else>{{values}}</span>
+        <span v-else>{{ values }}</span>
       </span>
     </transition>
     <slot></slot>
@@ -21,39 +22,41 @@
 </template>
 
 <script lang="ts">
-	import { defineComponent, Ref, ref } from 'vue'
-	import { props } from './props'
-	import Icon from '../icon'
+import { defineComponent, Ref, ref, computed, ComputedRef } from 'vue'
+import { props } from './props'
+import Icon from '../icon'
 
-	export default defineComponent({
-		name: 'VarBadge',
-		components: {
-			[Icon.name]: Icon
-		},
-		props,
-		setup(props) {
-			console.log(typeof props.value === 'number' && typeof props.maxValue === 'number')
-			console.log(props)
-			const values: Ref<number | string | null | undefined> = ref(null)
-			const dotPosition: Ref<string | null> = ref(null)
-			if (props.dot) {
-				values.value = null
-			} else if (typeof props.value === 'number' && typeof props.maxValue === 'number') {
-				values.value = props.value > props.maxValue ? `${props.maxValue}+` : props.value
-			} else {
-				values.value = props.value
-			}
-			if(props.position&&props.dot){
-				dotPosition.value = props.position.indexOf('right') !== -1 ? 'var-badge--dot-right' : props.position.indexOf('left') !== -1 ? 'var-badge--dot-left' : null
-			}
-			return {
-				values,
-        dotPosition
-			}
-		}
-	})
+export default defineComponent({
+  name: 'VarBadge',
+  components: {
+    [Icon.name]: Icon
+  },
+  inheritAttrs: false,
+  props,
+  setup(props) {
+    const values: ComputedRef<number | string | null> = computed(() => {
+      if (props.dot) {
+        return null
+      }
+      if (typeof props.value === 'number' && typeof props.maxValue === 'number') {
+        return (props.value > props.maxValue ? `${props.maxValue}+` : props.value)
+      }
+      return props.value
+    })
+    const dotPosition: ComputedRef<string | null> = computed(() => {
+      if (props.position && props.dot) {
+        return props.position.indexOf('right') !== -1 ? 'var-badge--dot-right' : props.position.indexOf('left') !== -1 ? 'var-badge--dot-left' : null
+      }
+      return null
+    })
+    return {
+      values,
+      dotPosition
+    }
+  }
+})
 </script>
 
 <style lang="less">
-  @import "./badge";
+@import "./badge";
 </style>
--- packages/varlet-ui/src/chip/Chip.vue
+++ packages/varlet-ui/src/chip/Chip.vue
@@ -6,55 +6,56 @@
       :class="[
 				`var-chip--${size}`,
 				block ? 'var--flex' : 'var--inline-flex',
-				{'var-chip--round':round},
 				plain ? `var-chip--plain-${type}` : `var-chip--${type}`,
-				plain ? `var-chip--plain` : `var-elevation--2`,
+				{ 'var-chip--round' : round,
+				  'var-chip--plain' : plain }
 			]"
-      :style="{
-        color: fontColor,
-        background:background,
-        borderColor: borderColor,
-        }"
+      :style=controlStyle
+      v-bind="$attrs"
     >
-        <slot name="left"></slot>
+      <span :class="[{ 'var-chip--left' : isLeft }]"><slot name="left"></slot></span>
 			<slot/>
-			<slot name="right"></slot>
+      <span :class="[{ 'var-chip--right' : isRight }]"><slot name="right"></slot></span>
 			<span v-if="closable " class="var-chip--close" @click="$props.onClose">
-        <var-icon :name="`${iconName ? iconName: 'close-circle'}`"></var-icon>
+        <var-icon :name="`${iconName ? iconName : 'close-circle'}`"></var-icon>
       </span>
 		</span>
   </transition>
 </template>
 
 <script lang="ts">
-	import Ripple from '../ripple'
-	import { defineComponent, Ref, ref } from 'vue'
-	import { props } from './props'
-	import Icon from '../icon'
+import Ripple from '../ripple'
+import { defineComponent, Ref, ref, computed, ComputedRef } from 'vue'
+import { props } from './props'
+import Icon from '../icon'
 
-	export default defineComponent({
-		name: 'VarChip',
-		components: {
-			[Icon.name]: Icon
-		},
-		directives: { Ripple },
-		props,
-		setup(props) {
-			const fontColor: Ref<string|null> = ref(null)
-			const background: Ref<string|null> = ref(null)
-			const borderColor: Ref<string|null> = ref(null)
-			fontColor.value = props.plain ? props.textColor || props.color || null : props.color ? props.textColor || '#fff' : null
-			background.value = !props.plain ? props.color || null : null
-			borderColor.value = props.plain ? props.color || null : null
-			return {
-				fontColor,
-				background,
-				borderColor
-			}
-		}
-	})
+export default defineComponent({
+  name: 'VarChip',
+  components: {
+    [Icon.name]: Icon
+  },
+  directives: { Ripple },
+  inheritAttrs: false,
+  props,
+  setup(props, { slots }) {
+    const isRight: Ref<boolean> = ref(false)
+    const isLeft: Ref<boolean> = ref(false)
+    isRight.value = !!slots.right
+    isLeft.value = !!slots.left
+    const controlStyle = computed(() => ({
+      color: props.plain ? props.textColor || props.color || null : props.color ? props.textColor || '#fff' : null,
+      background: !props.plain ? props.color || null : null,
+      borderColor: props.plain ? props.color || null : null
+    }))
+    return {
+      controlStyle,
+      isRight,
+      isLeft
+    }
+  }
+})
 </script>
 
 <style lang="less">
-  @import './chip';
+@import './chip';
 </style>
