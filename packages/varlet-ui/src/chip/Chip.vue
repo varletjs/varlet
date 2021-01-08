285048810b68492b8a194a79feac35fb122a8a0c
kanghuiyi <1193185379@qq.com>
2021-01-08 09:48:48 +0800

    fix:solve the problem of chip margin

--- packages/varlet-ui/src/chip/Chip.vue
+++ packages/varlet-ui/src/chip/Chip.vue
@@ -13,9 +13,11 @@
       :style=controlStyle
       v-bind="$attrs"
     >
-      <span :class="[{ 'var-chip--left' : isLeft }]"><slot name="left"></slot></span>
-			<slot/>
-      <span :class="[{ 'var-chip--right' : isRight }]"><slot name="right"></slot></span>
+      <slot name="left"></slot>
+			<span :class="[`var-chip--text-${size}`]">
+        <slot/>
+      </span>
+      <slot name="right"></slot>
 			<span v-if="closable " class="var-chip--close" @click="$props.onClose">
         <var-icon :name="`${iconName ? iconName : 'close-circle'}`"></var-icon>
       </span>
@@ -25,7 +27,7 @@
 
 <script lang="ts">
 import Ripple from '../ripple'
-import { defineComponent, Ref, ref, computed, ComputedRef } from 'vue'
+import { defineComponent, computed, ComputedRef } from 'vue'
 import { props } from './props'
 import Icon from '../icon'
 
@@ -37,20 +39,14 @@ export default defineComponent({
   directives: { Ripple },
   inheritAttrs: false,
   props,
-  setup(props, { slots }) {
-    const isRight: Ref<boolean> = ref(false)
-    const isLeft: Ref<boolean> = ref(false)
-    isRight.value = !!slots.right
-    isLeft.value = !!slots.left
+  setup(props) {
     const controlStyle = computed(() => ({
       color: props.plain ? props.textColor || props.color || null : props.color ? props.textColor || '#fff' : null,
       background: !props.plain ? props.color || null : null,
       borderColor: props.plain ? props.color || null : null
     }))
     return {
-      controlStyle,
-      isRight,
-      isLeft
+      controlStyle
     }
   }
 })
