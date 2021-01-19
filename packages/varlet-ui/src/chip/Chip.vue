5959bd47731bca8ceab2c1bdca493661926cf0e4
kanghuiyi <1193185379@qq.com>
2021-01-19 17:04:58 +0800

    refactor: simplify the code

--- packages/varlet-ui/src/chip/Chip.vue
+++ packages/varlet-ui/src/chip/Chip.vue
@@ -1,58 +1,66 @@
 <template>
-  <transition name="var-fade">
-    <span
-      v-ripple="{ disabled }"
-      class="var-chip var--box"
-      :class="[
+	<transition name="var-fade">
+		<span
+			v-ripple="{ disabled }"
+			class="var-chip var--box"
+			:class="[
 				`var-chip--${size}`,
 				block ? 'var--flex' : 'var--inline-flex',
 				plain ? `var-chip--plain-${type}` : `var-chip--${type}`,
-				{ 'var-chip--round' : round,
-				  'var-chip--plain' : plain }
+				{ 'var-chip--round': round, 'var-chip--plain': plain },
 			]"
-      :style='controlStyle()'
-      v-bind="$attrs"
-    >
-      <slot name="left"></slot>
+			:style="controlStyle()"
+			v-bind="$attrs"
+		>
+			<slot name="left"></slot>
 			<span :class="[`var-chip--text-${size}`]">
-        <slot/>
-      </span>
-      <slot name="right"></slot>
-			<span v-if="closable " class="var-chip--close" @click="$props.onClose">
-        <var-icon :name="`${iconName ? iconName : 'close-circle'}`"></var-icon>
-      </span>
+				<slot />
+			</span>
+			<slot name="right"></slot>
+			<span v-if="closable" class="var-chip--close" @click="$props.onClose">
+				<var-icon :name="`${iconName ? iconName : 'close-circle'}`"></var-icon>
+			</span>
 		</span>
-  </transition>
+	</transition>
 </template>
 
 <script lang="ts">
 import Ripple from '../ripple'
-import { defineComponent} from 'vue'
+import { defineComponent, Ref, ref } from 'vue'
 import { props } from './props'
 import Icon from '../icon'
 
 export default defineComponent({
-  name: 'VarChip',
-  components: {
-    [Icon.name]: Icon
-  },
-  directives: { Ripple },
-  inheritAttrs: false,
-  props,
-  setup(props) {
-    const controlStyle = () => {
-      return {
-        color: props.textColor ? props.textColor
-          : props.plain ? props.color || null
-            : props.color ? '#fff' : null,
-        background: !props.plain ? props.color || null : null,
-        borderColor: props.plain ? props.color || null : null
-      }
-    }
-    return {
-      controlStyle
-    }
-  }
+	name: 'VarChip',
+	components: {
+		[Icon.name]: Icon,
+	},
+	directives: { Ripple },
+	inheritAttrs: false,
+	props,
+	setup(props) {
+		const color: Ref<string | null> = ref(null)
+		if (props.textColor) {
+			// eslint-disable-next-line vue/no-setup-props-destructure
+			color.value = props.textColor
+		} else if (props.plain) {
+			color.value = props.color || null
+		} else if (props.color) {
+			color.value = '#fff'
+		} else {
+			color.value = null
+		}
+		const controlStyle = () => {
+			return {
+				color: color.value,
+				background: !props.plain ? props.color || null : null,
+				borderColor: props.plain ? props.color || null : null,
+			}
+		}
+		return {
+			controlStyle,
+		}
+	},
 })
 </script>
 
--- packages/varlet-ui/src/rate/Rate.vue
+++ packages/varlet-ui/src/rate/Rate.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="var-rate" ref="rate">
-    <div class="var-rate--content" v-for="(val,ind) in valueList" :key="val.score">
-      <var-icon
-        v-ripple="{disabled: readonly || disabled ? true : !ripple}"
-        :transition="1"
-        :name="`${val.type === 'full' ? icon : val.type === 'half' ? halfIcon : emptyIcon}`"
-        :style="{
-          color: val.color,
-          cursor: readonly ? 'default' : disabled?'not-allowed' :'pointer',
-          fontSize,
-          marginRight: ind + 1 !== count ? marginRight : 0}"
-        @click="handleClick(val.score,$event)"
-      />
-    </div>
-  </div>
+	<div class="var-rate" ref="rate">
+		<div
+			class="var-rate--content"
+			v-for="val in count"
+			:key="val"
+			v-ripple="{ disabled: readonly || disabled ? true : !ripple }"
+			:style="{
+				color: transformValue(val + 1).color,
+				cursor: readonly ? 'default' : disabled ? 'not-allowed' : 'pointer',
+				fontSize,
+				marginRight: val !== count ? marginRight : 0,
+			}"
+		>
+			<var-icon
+				:transition="1"
+				:name="`${
+					transformValue(val + 1).type === 'full'
+						? icon
+						: transformValue(val + 1).type === 'half'
+						? halfIcon
+						: emptyIcon
+				}`"
+				@click="handleClick(val + 1, $event)"
+			/>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -22,70 +33,51 @@ import { defineComponent, ref, Ref, watch } from 'vue'
 import { props } from './props'
 import Icon from '../icon'
 
-interface labelValue {
-  type: string,
-  score: number,
-  color: string
-}
-
 export default defineComponent({
-  name: 'VarRate',
-  components: {
-    [Icon.name]: Icon
-  },
-  directives: { Ripple },
-  inheritAttrs: false,
-  props,
-  setup(props) {
-    const valueList: Ref<labelValue[]> = ref([])
-    const transformValue = (index: number, score: number) => {
-      if (index <= score) {
-        return { type: 'full', score: index, color: props.disabled ? props.disabledColor : props.color }
-      }
-      if (props.half && index <= score + 0.5) {
-        return { type: 'half', score: index, color: props.disabled ? props.disabledColor : props.color }
-      }
-      return { type: 'empty', score: index, color: props.disabled ? props.disabledColor : props.emptyColor }
-    }
-    const calculation = (value: number) => {
-      valueList.value = []
-      for (let i = 1; i <= props.count; i++) {
-        valueList.value.push(transformValue(i, value))
-      }
-    }
-    calculation(props.modelValue)
-    const changeValue = (score: number, event: MouseEvent) => {
-      if (props.half) {
-        const { offsetWidth } = event.target as HTMLDivElement
-        if (event.offsetX <= Math.floor(offsetWidth / 2)) {
-          score -= 0.5
-        }
-      }
-      props['onUpdate:modelValue']?.(score)
-    }
-    const handleClick = (score: number, event: MouseEvent) => {
-      if (props.readonly || props.disabled) {
-        return
-      }
-      changeValue(score, event)
-      props.onChange?.(score)
-    }
-    watch(
-      () => props.modelValue,
-      (newValue) => {
-        calculation(newValue)
-      })
-    const fontSize: Ref<number | string> = ref(0)
-    fontSize.value = !isNaN(Number(props.size)) ? `${props.size}px` : props.size
-    const marginRight: Ref<number | string> = ref(0)
-    marginRight.value = !isNaN(Number(props.marginRight)) ? `${props.marginRight}px` : props.marginRight
-    return {
-      valueList,
-      handleClick,
-      fontSize,
-      marginRight
-    }
-  }
+	name: 'VarRate',
+	components: {
+		[Icon.name]: Icon,
+	},
+	directives: { Ripple },
+	inheritAttrs: false,
+	props,
+	setup(props) {
+		const transformValue = (index: number) => {
+			if (index <= props.modelValue) {
+				return { type: 'full', score: index, color: props.disabled ? props.disabledColor : props.color }
+			}
+			if (props.half && index <= props.modelValue + 0.5) {
+				return { type: 'half', score: index, color: props.disabled ? props.disabledColor : props.color }
+			}
+			return { type: 'empty', score: index, color: props.disabled ? props.disabledColor : props.emptyColor }
+		}
+		const changeValue = (score: number, event: MouseEvent) => {
+			if (props.half) {
+				const { offsetWidth } = event.target as HTMLDivElement
+				if (event.offsetX <= Math.floor(offsetWidth / 2)) {
+					score -= 0.5
+				}
+			}
+			props['onUpdate:modelValue']?.(score)
+		}
+		const handleClick = (score: number, event: MouseEvent) => {
+			if (props.readonly || props.disabled) {
+				return
+			}
+			changeValue(score, event)
+			props.onChange?.(score)
+		}
+		const fontSize: Ref<number | string> = ref(0)
+		fontSize.value = !isNaN(Number(props.size)) ? `${props.size}px` : props.size
+		const marginRight: Ref<number | string> = ref(0)
+		marginRight.value = !isNaN(Number(props.marginRight)) ? `${props.marginRight}px` : props.marginRight
+		return {
+			transformValue,
+			handleClick,
+			fontSize,
+			marginRight,
+		}
+	},
 })
 </script>
 
