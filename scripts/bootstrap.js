f6eaa119b83b3f607090a489b7f89e8f5a56a1ca
haoziqaq <357229046@qq.com>
2021-03-01 17:19:19 +0800

    fix(root): bootstrap脚本增加ui的compile操作

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -4,6 +4,7 @@ const path = require('path')
 const CWD = process.cwd()
 const PKG_CLI = path.resolve(CWD, './packages/varlet-cli')
 const PKG_ICONS = path.resolve(CWD, './packages/varlet-icons')
+const PKG_UI = path.resolve(CWD, './packages/varlet-ui')
 
 execa('yarn', ['build'], {
   cwd: PKG_CLI,
@@ -15,4 +16,10 @@ execa('yarn', ['build'], {
   cwd: PKG_ICONS,
 }).then(() => {
   console.log('@varlet/icons build success!')
+})
+
+execa('yarn', ['compile'], {
+  cwd: PKG_UI,
+}).then(() => {
+  console.log('@varlet/ui compile success!')
 })
