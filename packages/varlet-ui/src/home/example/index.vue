fbd0bf92fde96d419e85a1eaf7db7b6e272fe349
haoziqaq <357229046@qq.com>
2021-03-18 16:45:57 +0800

    docs(ui/home): 完成mobile索引页面组件目录显示
    affects: @varlet/ui

--- packages/varlet-ui/src/home/example/index.vue
+++ packages/varlet-ui/src/home/example/index.vue
@@ -1,17 +1,68 @@
 <template>
-  <p1>首页</p1>
+  <div class="space"></div>
+  <var-cell
+    v-for="component in components"
+    :key="component.text"
+  >
+    <template #extra>
+      <var-icon name="chevron-right" size="14" />
+    </template>
+    <template #default>
+      {{ component.text['zh-CN'] }}
+    </template>
+  </var-cell>
+  <div class="space"></div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import Cell from '../../cell'
+import Icon from '../../icon'
+import Ripple from '../../ripple'
+import { reactive, ref } from 'vue'
+import { watchLang, watchPlatform } from '../../utils/components'
 
-export default defineComponent({
-  name: 'Home',
-})
+const varletConfig = require('../../../varlet.config')
+
+export default {
+  name: 'HomeExample',
+  directives: { Ripple },
+  components: {
+    [Cell.name]: Cell,
+    [Icon.name]: Icon,
+  },
+  setup() {
+    const configComponents = varletConfig.pc.menu.filter((item) => !item.isTitle && !item.nonComponent)
+    const components = reactive(configComponents)
+    const lang = ref('zh-CN')
+    const platform = ref('mobile')
+
+    watchLang((newValue) => {
+      lang.value = newValue
+
+      console.log(lang.value)
+    })
+
+    watchPlatform((newValue) => {
+      platform.value = newValue
+
+      console.log(platform.value)
+    })
+
+    return {
+      components,
+      platform,
+      lang,
+    }
+  }
+}
 </script>
 
-<style scoped>
-.example {
-  background: antiquewhite;
+<style scoped lang="less">
+.space {
+  height: 5px;
+}
+
+.var-cell {
+  cursor: pointer;
 }
 </style>
