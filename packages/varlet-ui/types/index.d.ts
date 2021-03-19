d57ae90c48eb1221af946983d6687e5b0de5780d
haoziqaq <357229046@qq.com>
2021-03-19 15:12:26 +0800

    docs(ui/types): 构建类型索引
    affects: @varlet/ui

--- packages/varlet-ui/types/index.d.ts
+++ packages/varlet-ui/types/index.d.ts
@@ -1,13 +1,25 @@
+import { BackTop } from './BackTop'
 import { Button } from './Button'
+import { Cell } from './Cell'
 import { Col } from './Col'
+import { Collapse } from './Collapse'
+import { CollapseItem } from './CollapseItem'
+import { Countdown } from './Countdown'
 import { Dialog } from './Dialog'
 import { Image } from './Image'
+import { IndexAnchor } from './IndexAnchor'
+import { IndexBar } from './IndexBar'
 import { Lazy } from './Lazy'
+import { Loading } from './Loading'
 import { Menu } from './Menu'
 import { Popup } from './Popup'
+import { Progress } from './Progress'
+import { PullRefresh } from './PullRefresh'
 import { Ripple } from './Ripple'
 import { Row } from './Row'
+import { Slider } from './Slider'
 import { Sticky } from './Sticky'
+import { Switch } from './Switch'
 import { Tab } from './Tab'
 import { TabItem } from './TabItem'
 import { Tabs } from './Tabs'
@@ -19,16 +31,28 @@ import { App } from 'vue'
 export const install: (app: App) => void
 
 export {
+  BackTop,
   Button,
+  Cell,
   Col,
+  Collapse,
+  CollapseItem,
+  Countdown,
   Dialog,
   Image,
+  IndexAnchor,
+  IndexBar,
   Lazy,
+  Loading,
   Menu,
   Popup,
+  Progress,
+  PullRefresh,
   Ripple,
   Row,
+  Slider,
   Sticky,
+  Switch,
   Tab,
   TabItem,
   Tabs,
