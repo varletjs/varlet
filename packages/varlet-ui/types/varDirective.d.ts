cd1a4028900ab91bc3aa0692d1f0653ea8f0bdd5
moon <362652565@qq.com>
2021-04-30 15:54:33 +0800

    fix(types): vite build typescript error

--- packages/varlet-ui/types/varDirective.d.ts
+++ packages/varlet-ui/types/varDirective.d.ts
@@ -4,9 +4,9 @@ import { DirectiveBinding } from '@vue/runtime-core'
 export class VarDirective {
   static install(app: App): void
 
-  static mounted(el: HTMLElement, binding: DirectiveBinding)
+  static mounted(el: HTMLElement, binding: DirectiveBinding): void
 
-  static updated(el: HTMLElement, binding: DirectiveBinding)
+  static updated(el: HTMLElement, binding: DirectiveBinding): void
 
-  static unmounted(el: HTMLElement, binding: DirectiveBinding)
+  static unmounted(el: HTMLElement, binding: DirectiveBinding): void
 }
