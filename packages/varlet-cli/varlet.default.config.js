28c97453752b96618ca8289cb4af5c4ceba0b25c
34982452 <34982452@qq.com>
2021-05-28 11:01:00 +0800

    fix(cli/site/pc): add browser title toggle both  in english and chinese  fix(varletconfig): Merge less commonly used configurations   into default configuration items

--- packages/varlet-cli/varlet.default.config.js
+++ packages/varlet-cli/varlet.default.config.js
@@ -1,24 +1,33 @@
 module.exports = {
+  name: 'Varlet',
   namespace: 'var',
   host: 'localhost',
-  title: 'Varlet',
+  title: 'VARLET',
   port: 8080,
   pc: {
-    title: 'Varlet 组件库文档',
-    description: 'Varlet 组件库文档',
-    logo: 'https://cn.vuejs.org/images/logo.png',
-    redirect: undefined,
+    title: {
+      'zh-CN': 'Varlet 面向Vue3的Material风格移动端组件库',
+      'en-US': 'Material design mobile components built for Vue3',
+    },
+    description: 'Varlet 面向Vue3的Material风格移动端组件库',
+    logo: 'https://varlet.gitee.io/varlet-ui/varlet_icon.png',
+    redirect: '/zh-CN/home',
     header: {
-      i18nButton: {
-        zh_CN: 'En',
-        en_US: '中文',
+      logo: 'https://varlet.gitee.io/varlet-ui/varlet_icon.png',
+      search: {
+        'zh-CN': '搜索文档...',
+        'en-US': 'Search...',
+      },
+      language: {
+        'zh-CN': '中文',
+        'en-US': 'English',
       },
     },
   },
   mobile: {
-    title: 'Varlet 组件库示例',
-    description: 'Varlet 组件库示例',
-    logo: 'https://cn.vuejs.org/images/logo.png',
-    redirect: undefined,
+    title: 'Varlet 面向Vue3的Material风格移动端组件库',
+    description: 'Varlet 面向Vue3的Material风格移动端组件库',
+    logo: 'https://varlet.gitee.io/varlet-ui/varlet_icon.png',
+    redirect: '/home',
   },
 }
