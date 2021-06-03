8cc50f50b3361cd5db68caafb40e9d991bb50899
haoziqaq <357229046@qq.com>
2021-06-03 15:51:34 +0800

    refactor(extension): support jsx tsx
    affects: varlet-vscode-extension

--- packages/varlet-vscode-extension/src/extension.ts
+++ packages/varlet-vscode-extension/src/extension.ts
@@ -2,7 +2,13 @@ import * as vscode from 'vscode'
 import siteMap from './siteMap'
 
 const DOC = 'https://varlet.gitee.io/varlet-ui/#/zh-CN'
-const NAMESPACE_RE = /(?<=<var-)([\w-]+)(?=[^>]*\/?>)/g
+const LINK_RE = /(?<=<var-)([\w-]+)(?=[^>]*\/?>)/g
+const BIG_CAMELIZE_RE = /(?<=<Var)([\w-]+)(?=[^>]*\/?>)/g
+
+function parseToLink(str: string) {
+  str = str.replace(str.charAt(0), str.charAt(0).toLowerCase())
+  return str.replace(/([a-z])([A-Z])/g, (_, p1, p2) => p1 + '-' + p2.toLowerCase())
+}
 
 function camelize(str: string): string {
   return str.replace(/-(\w)/g, (_: any, p: string) => p.toUpperCase())
@@ -14,9 +20,11 @@ function bigCamelize(str: string): string {
 
 function provideHover(document: vscode.TextDocument, position: vscode.Position) {
   const line = document.lineAt(position)
-  const components = line.text.match(NAMESPACE_RE)
+  const linkComponents = line.text.match(LINK_RE) ?? []
+  const bigCamelizeComponents = line.text.match(BIG_CAMELIZE_RE) ?? []
+  const components = [...new Set([...linkComponents, ...bigCamelizeComponents.map(parseToLink)])]
 
-  if (Array.isArray(components)) {
+  if (components.length) {
     const contents = components
       .filter((component) => siteMap[component])
       .map((component) => `[查看${bigCamelize(component)}组件Varlet官方文档](${DOC}${siteMap[component]})`)
@@ -26,7 +34,7 @@ function provideHover(document: vscode.TextDocument, position: vscode.Position) 
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.languages.registerHoverProvider(['vue'], {
+    vscode.languages.registerHoverProvider(['vue', 'typescript', 'javascript', 'javascriptreact', 'typescriptreact'], {
       provideHover,
     })
   )
