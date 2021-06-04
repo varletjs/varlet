5dc3d6b30c7defe01168f1b85cb13fa37ec5c5dc
wf-1202 <401073485@qq.com>
2021-06-04 18:14:51 +0800

    feat(ui/card): add Card component

---
+++ packages/varlet-ui/src/card/example/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <app-type>{{ pack.basicUsage }}</app-type>
+  <var-card :title="pack.title" :desc="pack.description" />
+  <app-type>{{ pack.showSubtitle }}</app-type>
+  <var-card :title="pack.title" :subtitle="pack.subtitle" :desc="pack.description" />
+  <app-type>{{ pack.showImage }}</app-type>
+  <var-card :title="pack.title" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
+  <app-type>{{ pack.useSlot }}</app-type>
+  <var-card
+    :title="pack.title"
+    :subtitle="pack.subtitle"
+    :desc="pack.description"
+    src="https://varlet.gitee.io/varlet-ui/cat.jpg"
+  >
+    <template #extra>
+      <var-button text outline type="primary">{{ pack.button }}</var-button>
+    </template>
+  </var-card>
+</template>
+
+<script>
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import Button from '../../button'
+import Card from '..'
+import { pack, use } from './locale'
+import { watchLang } from '../../utils/components'
+
+export default {
+  name: 'CardExample',
+  components: {
+    [Card.name]: Card,
+    [Button.name]: Button,
+    AppType,
+  },
+  setup() {
+    watchLang(use)
+    return {
+      pack,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.example {
+  background: antiquewhite;
+}
+</style>
---
+++ packages/varlet-ui/src/card/example/locale/en-US.ts
@@ -0,0 +1,10 @@
+export default {
+  basicUsage: 'Basic Usage',
+  title: 'Title',
+  showSubtitle: 'Show Subtitle',
+  subtitle: 'Subtitle',
+  description: 'Description',
+  showImage: 'Show Image',
+  useSlot: 'Use Slot',
+  button: 'Use Button',
+}
