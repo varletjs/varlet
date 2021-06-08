8ff14a61c1d650d8d9bbfc80cafe389d74cf126f
wf-1202 <401073485@qq.com>
2021-06-08 17:14:12 +0800

    test(ui/card): add test case for web

--- packages/varlet-ui/src/card/__tests__/index.spec.js
+++ packages/varlet-ui/src/card/__tests__/index.spec.js
@@ -1,14 +1,57 @@
 import example from '../example'
 import Card from '..'
+import VarCard from '../Card'
 import { mount } from '@vue/test-utils'
 import { createApp } from 'vue'
 
 test('test card example', () => {
   const wrapper = mount(example)
   expect(wrapper.html()).toMatchSnapshot()
+  wrapper.unmount()
 })
 
 test('test card plugin', () => {
   const app = createApp({}).use(Card)
   expect(app.component(Card.name)).toBeTruthy()
+})
+
+test('test card props', async () => {
+  const wrapper = mount(VarCard, {
+    props: {
+      title: 'This is Card',
+      description: 'This is description',
+      subtitle: 'This is subtitle',
+      src: 'https://varlet.gitee.io/varlet-ui/cat.jpg',
+      fit: 'cover',
+      height: '200px',
+      alt: 'This is an image',
+      elevation: '2',
+      ripple: true,
+    },
+    slots: {
+      extra: () => 'text',
+    },
+  })
+
+  expect(wrapper.html()).toMatchSnapshot()
+  wrapper.unmount()
+})
+
+test('test card onClick with null callback', () => {
+  const wrapper = mount(VarCard)
+  wrapper.trigger('click')
+  wrapper.unmount()
+})
+
+test('test card onClick', () => {
+  const onClick = jest.fn()
+  const wrapper = mount(VarCard, {
+    props: {
+      onClick,
+    },
+  })
+
+  wrapper.trigger('click')
+  expect(onClick).toHaveBeenCalledTimes(1)
+  wrapper.unmount()
 })
