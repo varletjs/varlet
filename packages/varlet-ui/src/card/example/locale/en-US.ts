ecac9730e81072e755b567b630b5a323be293e47
Grizz <panan521@qq.com>
2021-06-09 19:02:51 +0800

    docs(ui/card): 英文文档和api项样式修改

--- packages/varlet-ui/src/card/example/locale/en-US.ts
+++ packages/varlet-ui/src/card/example/locale/en-US.ts
@@ -1,10 +1,10 @@
 export default {
   basicUsage: 'Basic Usage',
-  title: 'You have only one life',
-  showSubtitle: 'Show Subtitle',
-  subtitle: 'Go where you want to go',
+  title: 'Little Prince',
+  showSubtitle: 'Show Subtitlewhere',
+  subtitle: 'little prince from',
   description:
-    'There are moments in life when you miss someone so much that you just want to pick them from your dreams and hug them for real!',
+    'It took me a long time to learn where he came from. The little prince, whoasked me so many questions, never seemed to hear the ones I asked him. Itwas from words dropped by chance that, little by little, everything wasrevealed to me.',
   showImage: 'Show Image',
   useSlot: 'Use Slot',
   button: 'Use Button',
