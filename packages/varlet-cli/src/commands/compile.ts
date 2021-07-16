7fc61deb9eca3bf30a4ecf1d37f531d2c1b154e8
BeADre <beadre_wang@163.com>
2021-07-16 16:22:28 +0800

    fix: adjust the execution position of the remove function

--- packages/varlet-cli/src/commands/compile.ts
+++ packages/varlet-cli/src/commands/compile.ts
@@ -1,12 +1,12 @@
 import ora from 'ora'
 import { remove } from 'fs-extra'
-import { ES_DIR, UMD_DIR } from '../shared/constant'
+import { ES_DIR, HL_DIR, UMD_DIR } from '../shared/constant'
 import { compileModule } from '../compiler/compileModule'
 import { compileTemplateHighlight } from '../compiler/compileTemplateHighlight'
 import { compileTypes } from '../compiler/compileTypes'
 
 export function removeDir() {
-  return Promise.all([remove(ES_DIR), remove(UMD_DIR)])
+  return Promise.all([remove(ES_DIR), remove(HL_DIR), remove(UMD_DIR)])
 }
 
 export async function compile() {
