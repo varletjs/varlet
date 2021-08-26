0dd413e0569613db9e263a9a3f2e6e2989e52794
BeADre <beadre_wang@163.com>
2021-08-26 17:52:15 +0800

    refactor(markdown-loader): optimize code

--- packages/varlet-markdown-loader/index.js
+++ packages/varlet-markdown-loader/index.js
@@ -1,14 +1,21 @@
 const markdown = require('markdown-it')
 const hljs = require('highlight.js')
-const cardWrapper = require('./src/cardWrapper.js')
+
+function cardWrapper(html) {
+  const group = html.replace(/<h3/g, ':::<h3').replace(/<h2/g, ':::<h2').split(':::')
+
+  return group.map((fragment) => (fragment.includes('<h3') ? `<div class="card">${fragment}</div>` : fragment)).join('')
+}
 
 function highlight(str, lang) {
   if (lang && hljs.getLanguage(lang)) {
-    return (
-      '<pre class="hljs"><code><link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.2/styles/color-brewer.min.css">' +
-      hljs.highlight(lang, str, true).value +
-      '</code></pre>'
-    )
+    return `<pre>
+         <code>
+           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.2/styles/color-brewer.min.css">
+           ${hljs.highlight(lang, str, true).value}
+         </code>
+       </pre>
+      `
   }
 
   return ''
@@ -22,7 +29,7 @@ function markLoader(source) {
   })
   const html = cardWrapper(md.render(source))
 
-  return `<template>\n<div class="varlet-site-doc" v-pre>${html}</div>\n</template>\n`
+  return `<template><div class="varlet-site-doc" v-pre>${html}</div></template>`
 }
 
 module.exports = markLoader
