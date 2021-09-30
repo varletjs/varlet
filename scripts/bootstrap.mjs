1275a84e83ea8a9533637bc7302f64e8e5cf39bf
haoziqaq <357229046@qq.com>
2021-10-01 02:25:54 +0800

    chore(root): remove build ui progress from bootstrap script

--- scripts/bootstrap.mjs
+++ scripts/bootstrap.mjs
@@ -1,5 +1,4 @@
-import { buildCli, buildIcons, buildUI, runTask } from './build.mjs'
+import { buildCli, buildIcons, runTask } from './build.mjs'
 ;(async () => {
   await Promise.all([runTask('cli', buildCli), runTask('icons', buildIcons)])
-  await runTask('ui', () => buildUI(true))
 })()
