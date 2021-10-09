1b164d7f464d7c48b78cc17271a41170f61f53c6
haoziqaq <357229046@qq.com>
2021-10-09 15:44:50 +0800

    fix(cli): use vite internal printUrls instead of logger
    affects: @varlet/cli

--- packages/varlet-cli/src/commands/dev.ts
+++ packages/varlet-cli/src/commands/dev.ts
@@ -1,12 +1,10 @@
-import address from 'address'
-import logger from '../shared/logger'
 import { createServer } from 'vite'
 import { ensureDirSync } from 'fs-extra'
 import { SRC_DIR } from '../shared/constant'
 import { buildSiteEntry } from '../compiler/compileSiteEntry'
 import { getDevConfig } from '../config/vite.config'
 import { getVarletConfig } from '../config/varlet.config'
-import { get, merge } from 'lodash'
+import { merge } from 'lodash'
 
 export async function dev(cmd: { force?: boolean }) {
   process.env.NODE_ENV = 'development'
@@ -21,12 +19,5 @@ export async function dev(cmd: { force?: boolean }) {
 
   await server.listen()
 
-  const host = get(varletConfig, 'host')
-  const ipv4 = address.ip()
-  const { port } = server.config.server
-
-  logger.success(`\
-Server running at
-Local: http://${host}:${port}
-Network: http://${ipv4}:${port}`)
+  server.printUrls()
 }
