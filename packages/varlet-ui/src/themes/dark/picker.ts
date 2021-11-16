2d404d7c779d2df292bc6e6fe5521249740b1526
classmatewu <171755422@qq.com>
2021-11-17 01:22:50 +0800

    feat(picker): picker support dark mode
    affects: @varlet/ui

---
+++ packages/varlet-ui/src/themes/dark/picker.ts
@@ -0,0 +1,4 @@
+export default {
+  '--picker-background': '#272727',
+  '--picker-mask-background-image': `linear-gradient(180deg, hsl(0deg 0% 15%), hsl(0deg 7% 14% / 40%)), linear-gradient(0deg, hsl(0deg 0% 15%), hsl(0deg 7% 14% / 40%))`,
+}
