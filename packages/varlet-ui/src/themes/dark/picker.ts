c7304b37128096cc5a4b86e275a07de0015f32b7
BeADre <beadre_wang@163.com>
2021-11-17 09:41:47 +0800

    fix(picker): optimize dark mode

--- packages/varlet-ui/src/themes/dark/picker.ts
+++ packages/varlet-ui/src/themes/dark/picker.ts
@@ -1,4 +1,6 @@
 export default {
   '--picker-background': '#272727',
+  '--picker-cancel-button-text-color': '#aaa',
+  '--picker-picked-border': '1px solid rgba(255, 255, 255, 0.12)',
   '--picker-mask-background-image': `linear-gradient(180deg, hsl(0deg 0% 15%), hsl(0deg 7% 14% / 40%)), linear-gradient(0deg, hsl(0deg 0% 15%), hsl(0deg 7% 14% / 40%))`,
 }
