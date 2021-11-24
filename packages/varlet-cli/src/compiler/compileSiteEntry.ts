3363578eb2763282fd3c274ab75374cbff94c92b
haoziqaq <357229046@qq.com>
2021-11-24 20:08:33 +0800

    fix(cli): add ts ignore to generate routes
    affects: @varlet/cli

--- packages/varlet-cli/src/compiler/compileSiteEntry.ts
+++ packages/varlet-cli/src/compiler/compileSiteEntry.ts
@@ -91,6 +91,7 @@ export async function buildMobileSiteRoutes() {
     (examplePath) => `
   {
     path: '${getExampleRoutePath(examplePath)}',
+    // @ts-ignore
     component: () => import('${examplePath}')
   }\
 `
