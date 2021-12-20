b48d51df92612191cd838f97120abe4103161aed
sundada <956593305@qq.com>
2021-12-20 20:57:51 +0800

    refactor(ui/collapse-menu-item): destructuring assignment from props

--- packages/varlet-ui/src/collapse-menu-item/CollapseMenuItem.tsx
+++ packages/varlet-ui/src/collapse-menu-item/CollapseMenuItem.tsx
@@ -11,38 +11,47 @@ export default defineComponent({
   directives: { Ripple },
   props,
   setup(props, { slots }) {
-    const renderIcon = () => {
-      if (slots.icon) {
-        return slots.icon()
-      }
-      if (isFunction(props.icon)) {
-        return props.icon(h)
-      }
-      if (isPlainObject(props.icon)) {
-        return <Icon {...props.icon}></Icon>
-      }
-      if (isString(props.icon)) {
-        return <Icon name={props.icon} />
-      }
-    }
-    const renderLabel = () => {
-      if (slots.label) {
-        return slots.label()
-      }
-      if (isFunction(props.label)) {
-        return props.label(h)
-      }
-      return <div>{props.label}</div>
-    }
-    const handleClick = () => {
-      if (props.disabled) {
-        return
-      }
-      console.log('李航宇就靠你了')
-    }
     return () => {
       const prefix = 'var-collapse-menu-item'
-      const { disabled, key } = props
+      const { disabled, key, icon, label } = props
+
+      const renderIcon = () => {
+        if (slots.icon) {
+          return slots.icon()
+        }
+
+        if (isFunction(icon)) {
+          return icon()
+        }
+
+        if (isPlainObject(icon)) {
+          return <Icon {...icon} />
+        }
+
+        if (isString(icon)) {
+          return <Icon name={icon} />
+        }
+      }
+
+      const renderLabel = () => {
+        if (slots.label) {
+          return slots.label()
+        }
+
+        if (isFunction(label)) {
+          return label()
+        }
+
+        return <div>{label}</div>
+      }
+
+      const handleClick = () => {
+        if (props.disabled) {
+          return
+        }
+        console.log('李航宇就靠你了')
+      }
+
       return (
         <div
           onClick={handleClick}
