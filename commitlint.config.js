8268f56ce24f664d6965995e02f70deefba8d4ac
linkscope <linkscope@foxmail.com>
2021-12-28 13:43:22 +0800

    chore: commit message expand

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'dx',
+        'refactor',
+        'perf',
+        'test',
+        'workflow',
+        'build',
+        'ci',
+        'chore',
+        'types',
+        'wip',
+        'release',
+        'deps',
+      ],
+    ],
+  },
 }
