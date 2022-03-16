1b61cddf53ebca76ad6d86240f25e154cb462b03
BeADre <beadre_wang@163.com>
2022-03-16 16:21:04 +0800

    revert(ui/index-bar): revert fix scroll getter function
    This reverts commit d3dd14f88b79c20a3ba1b2b159dca974b4d2e5fe.

--- packages/varlet-ui/src/utils/elements.ts
+++ packages/varlet-ui/src/utils/elements.ts
@@ -14,10 +14,6 @@ export function getTop(element: HTMLElement): number {
 }
 
 export function getScrollTop(element: Element | Window): number {
-  if (element === document.documentElement || element === document.body) {
-    return document.body.scrollTop || document.documentElement.scrollTop
-  }
-
   const top = 'scrollTop' in element ? element.scrollTop : element.pageYOffset
 
   // iOS scroll bounce cause minus scrollTop
@@ -25,10 +21,6 @@ export function getScrollTop(element: Element | Window): number {
 }
 
 export function getScrollLeft(element: Element | Window): number {
-  if (element === document.documentElement || element === document.body) {
-    return document.body.scrollLeft || document.documentElement.scrollLeft
-  }
-
   const left = 'scrollLeft' in element ? element.scrollLeft : element.pageXOffset
 
   return Math.max(left, 0)
