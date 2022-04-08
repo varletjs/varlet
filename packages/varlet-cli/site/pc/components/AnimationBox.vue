7b968b827a873ee8aa678b54636713a58681da61
wegi8 <43628500+wegi8@users.noreply.github.com>
2022-04-08 09:42:30 +0800

    refactor: review the home page && completion the language && move the body css into the `varlet-site` className (#413)
    * chore: init rootRoute && pages config
    
    * refactor: update the compileSiteEntry
    
    i forget it
    
    * refactor: review the home page && completion the language && move the layout body css in the htmlTemplate
    
    * feat: the animation logo integrated

---
+++ packages/varlet-cli/site/pc/components/AnimationBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="varletLogoAnimationRef"></div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref, useAttrs } from "vue"
+import { animationBoxData, animationEl } from "../floating"
+export default defineComponent({
+  name: "AnimationBox",
+  setup() {
+    onMounted(() => {
+      animationEl.value = varletLogoAnimationRef.value;
+    })
+
+    const varletLogoAnimationRef = ref<HTMLElement>();
+    const attrs = useAttrs();
+    animationBoxData.attrs = attrs;
+
+    return {
+      varletLogoAnimationRef
+    }
+  }
+})
+</script>
+<style></style>
