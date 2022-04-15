de6d36c24571590225c5bdcc75fed6ca8d1a608c
EmberSpirit <85718018+wangKBweb@users.noreply.github.com>
2022-04-15 22:34:21 +0800

    docs(ui/rate): use the setup script for exmple and docs (#436)
    * docs(ui/counter): use the setup script for exmple and docs
    
    * docs(ui/switch): use the setup script for example and docs
    
    * docs: remove unnecessary code
    
    * docs(ui/switch): modify code format in example
    
    * docs(ui/rate): use the setup script for exmple and docs

--- packages/varlet-ui/src/rate/example/index.vue
+++ packages/varlet-ui/src/rate/example/index.vue
@@ -1,35 +1,70 @@
+<script setup>
+import VarRate from '..'
+import VarSnackbar from '../../snackbar'
+import AppType from '@varlet/cli/site/mobile/components/AppType.vue'
+import dark from '../../themes/dark'
+import { reactive, toRefs } from 'vue'
+import { pack, use } from './locale'
+import { watchLang, watchDarkMode } from '@varlet/cli/site/utils'
+
+const scores = reactive({
+  score: 3,
+  score1: 3,
+  score2: 3,
+  score3: 3,
+  score4: 3,
+  score5: 3,
+  score6: 3.5,
+  score7: 3,
+  score8: 3,
+  score9: 3,
+  score10: 3,
+  score11: 3,
+})
+
+const handleChange = (score) => {
+  VarSnackbar({
+    content: `click ${score}`,
+    position: 'top',
+  })
+}
+
+watchLang(use)
+watchDarkMode(dark)
+</script>
+
 <template>
   <app-type>{{ pack.baseRating }}</app-type>
-  <var-rate v-model="score" />
+  <var-rate v-model="scores.score" />
 
   <app-type>{{ pack.customizeTheTotalNumberOfRatingICONS }}</app-type>
-  <var-rate v-model="score1" :count="8" />
+  <var-rate v-model="scores.score1" :count="8" />
 
   <app-type>{{ pack.customizeTheIconColor }}</app-type>
-  <var-rate v-model="score2" color="#9c27b0" empty-color="#d199da" />
-  <var-rate v-model="score2" color="#e91e63" empty-color="#f48fb1" />
-  <var-rate v-model="score2" color="#4caf50" empty-color="#a5d6a7" />
-  <var-rate v-model="score2" color="#3f51b5" empty-color="#9fa8da" />
+  <var-rate v-model="scores.score2" color="#9c27b0" empty-color="#d199da" />
+  <var-rate v-model="scores.score2" color="#e91e63" empty-color="#f48fb1" />
+  <var-rate v-model="scores.score2" color="#4caf50" empty-color="#a5d6a7" />
+  <var-rate v-model="scores.score2" color="#3f51b5" empty-color="#9fa8da" />
 
   <app-type>{{ pack.customizeRatingIconStyles }}</app-type>
-  <var-rate v-model="score3" icon="heart" empty-icon="heart-outline" color="red"></var-rate>
+  <var-rate v-model="scores.score3" icon="heart" empty-icon="heart-outline" color="red"></var-rate>
 
   <app-type>{{ pack.customizeRatingIconSize }}</app-type>
-  <var-rate v-model="score4" :size="16" />
-  <var-rate v-model="score4" :size="18" />
-  <var-rate v-model="score4" :size="20" />
-  <var-rate v-model="score4" />
+  <var-rate v-model="scores.score4" :size="16" />
+  <var-rate v-model="scores.score4" :size="18" />
+  <var-rate v-model="scores.score4" :size="20" />
+  <var-rate v-model="scores.score4" />
 
   <app-type>{{ pack.customIconSpacing }}</app-type>
-  <var-rate v-model="score5" :gap="2" />
-  <var-rate v-model="score5" />
-  <var-rate v-model="score5" :gap="6" />
-  <var-rate v-model="score5" :gap="8" />
+  <var-rate v-model="scores.score5" :gap="2" />
+  <var-rate v-model="scores.score5" />
+  <var-rate v-model="scores.score5" :gap="6" />
+  <var-rate v-model="scores.score5" :gap="8" />
 
   <app-type>{{ pack.useHalfAStar }}</app-type>
-  <var-rate v-model="score6" :count="8" half />
+  <var-rate v-model="scores.score6" :count="8" half />
   <var-rate
-    v-model="score6"
+    v-model="scores.score6"
     :count="8"
     icon="heart"
     half-icon="heart-half-full"
@@ -39,72 +74,22 @@
   ></var-rate>
 
   <app-type>{{ pack.disableTheRating }}</app-type>
-  <var-rate v-model="score7" disabled disabled-color="#bbbbbb" />
+  <var-rate v-model="scores.score7" disabled disabled-color="#bbbbbb" />
 
   <app-type>{{ pack.readonlyRating }}</app-type>
-  <var-rate v-model="score8" readonly />
+  <var-rate v-model="scores.score8" readonly />
 
   <app-type>{{ pack.waterRippleIsProhibited }}</app-type>
-  <var-rate v-model="score9" :ripple="false" />
+  <var-rate v-model="scores.score9" :ripple="false" />
 
   <app-type>{{ pack.listeningForClickEvents }}</app-type>
-  <var-rate v-model="score10" @change="handleChange" />
+  <var-rate v-model="scores.score10" @change="handleChange" />
 
   <app-type>{{ pack.validate }}</app-type>
-  <var-rate :rules="[(v) => v >= 3 || pack.rateMessage]" v-model="score11" />
+  <var-rate :rules="[(v) => v >= 3 || pack.rateMessage]" v-model="scores.score11" />
 </template>
 
-<script>
-import VarRate from '..'
-import VarSnackbar from '../../snackbar'
-import AppType from '@varlet/cli/site/mobile/components/AppType.vue'
-import dark from '../../themes/dark'
-import { reactive, toRefs } from 'vue'
-import { pack, use } from './locale'
-import { watchLang, watchDarkMode } from '@varlet/cli/site/utils'
-
-export default {
-  name: 'RateExample',
-  components: {
-    VarRate,
-    AppType,
-  },
-  setup() {
-    const scores = reactive({
-      score: 3,
-      score1: 3,
-      score2: 3,
-      score3: 3,
-      score4: 3,
-      score5: 3,
-      score6: 3.5,
-      score7: 3,
-      score8: 3,
-      score9: 3,
-      score10: 3,
-      score11: 3,
-    })
-
-    const handleChange = (score) => {
-      VarSnackbar({
-        content: `click ${score}`,
-        position: 'top',
-      })
-    }
-
-    watchLang(use)
-    watchDarkMode(dark)
-
-    return {
-      handleChange,
-      pack,
-      ...toRefs(scores),
-    }
-  },
-}
-</script>
-
-<style scoped>
+<style>
 .var-rate {
   margin-bottom: 10px;
 }
