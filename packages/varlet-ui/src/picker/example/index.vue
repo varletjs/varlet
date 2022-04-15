3f1d80375ace58b1e7c2059c568d04bfb8d850ce
meetwhere-dev <65750209+meetwhere-dev@users.noreply.github.com>
2022-04-16 01:13:26 +0800

    docs(ui/picker): use the setup script for exmple and docs (#438)

--- packages/varlet-ui/src/picker/example/index.vue
+++ packages/varlet-ui/src/picker/example/index.vue
@@ -1,3 +1,43 @@
+<script setup>
+import Picker from '../index'
+import VarPicker from '../Picker'
+import VarButton from '../../button'
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import area from '../../../json/area.json'
+import dark from '../../themes/dark'
+import { ref } from 'vue'
+import { use, pack } from './locale'
+import { watchLang, watchDarkMode } from '@varlet/cli/site/utils'
+
+const columns = ref([Array.from({ length: 20 }).map((_, index) => index)])
+
+const columns2 = ref([
+  Array.from({ length: 20 }).map((_, index) => index),
+  Array.from({ length: 20 }).map((_, index) => index),
+  Array.from({ length: 20 }).map((_, index) => index),
+])
+
+const columns3 = ref(area)
+
+const picker = async () => {
+  await Picker(columns.value)
+}
+
+const picker2 = async () => {
+  await Picker(columns2.value)
+}
+
+const picker3 = async () => {
+  await Picker({
+    cascade: true,
+    columns: columns3.value,
+  })
+}
+
+watchLang(use)
+watchDarkMode(dark)
+</script>
+
 <template>
   <app-type>{{ pack.functionCall }}</app-type>
   <var-button type="primary" style="margin-bottom: 10px" block @click="picker">{{ pack.singlePicker }}</var-button>
@@ -9,62 +49,3 @@
   <var-picker style="margin-bottom: 14px" :columns="columns2" />
   <var-picker style="margin-bottom: 14px" cascade :columns="columns3" />
 </template>
-
-<script>
-import Picker from '../index'
-import VarButton from '../../button'
-import AppType from '@varlet/cli/site/mobile/components/AppType'
-import area from '../../../json/area.json'
-import dark from '../../themes/dark'
-import { ref } from 'vue'
-import { use, pack } from './locale'
-import { watchLang, watchDarkMode } from '@varlet/cli/site/utils'
-
-export default {
-  name: 'PickerExample',
-  components: {
-    VarPicker: Picker.Component,
-    VarButton,
-    AppType,
-  },
-  setup() {
-    const columns = ref([Array.from({ length: 20 }).map((_, index) => index)])
-
-    const columns2 = ref([
-      Array.from({ length: 20 }).map((_, index) => index),
-      Array.from({ length: 20 }).map((_, index) => index),
-      Array.from({ length: 20 }).map((_, index) => index),
-    ])
-
-    const columns3 = ref(area)
-
-    const picker = async () => {
-      await Picker(columns.value)
-    }
-
-    const picker2 = async () => {
-      await Picker(columns2.value)
-    }
-
-    const picker3 = async () => {
-      await Picker({
-        cascade: true,
-        columns: columns3.value,
-      })
-    }
-
-    watchLang(use)
-    watchDarkMode(dark)
-
-    return {
-      pack,
-      columns,
-      columns2,
-      columns3,
-      picker,
-      picker2,
-      picker3,
-    }
-  },
-}
-</script>
