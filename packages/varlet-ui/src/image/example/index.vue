98adb054b1852c8c46f7cb53d572b04dda20e188
wegi8 <18272190626@163.com>
2022-04-17 07:48:25 +0800

    docs(ui/image): use the setup script for example and docs, update the snap

--- packages/varlet-ui/src/image/example/index.vue
+++ packages/varlet-ui/src/image/example/index.vue
@@ -1,32 +1,42 @@
+<script setup>
+import VarImage from '..'
+import AppType from '@varlet/cli/site/mobile/components/AppType'
+import VarRow from '../../row'
+import { watchLang } from '@varlet/cli/site/utils'
+import { use, pack } from './locale'
+
+watchLang(use)
+</script>
+
 <template>
   <app-type>{{ pack.basicUsage }}</app-type>
   <var-image src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
 
   <app-type>{{ pack.fitMode }}</app-type>
   <var-row>
-    <div class="fit-item">
+    <div class="image-example-fit-item">
       <var-image width="85px" height="85px" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
-      <div>fill</div>
+      <div class="image-example-margin-bottom">fill</div>
     </div>
 
-    <div class="fit-item">
+    <div class="image-example-fit-item">
       <var-image width="85px" height="85px" fit="cover" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
-      <div>cover</div>
+      <div class="image-example-margin-bottom">cover</div>
     </div>
 
-    <div class="fit-item">
+    <div class="image-example-fit-item">
       <var-image width="85px" height="85px" fit="contain" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
-      <div>contain</div>
+      <div class="image-example-margin-bottom">contain</div>
     </div>
 
-    <div class="fit-item">
+    <div class="image-example-fit-item">
       <var-image width="85px" height="85px" fit="none" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
-      <div>none</div>
+      <div class="image-example-margin-bottom">none</div>
     </div>
 
-    <div class="fit-item">
+    <div class="image-example-fit-item">
       <var-image width="85px" height="85px" fit="scale-down" src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
-      <div>scale-down</div>
+      <div class="image-example-margin-bottom">scale-down</div>
     </div>
   </var-row>
 
@@ -51,36 +61,14 @@
   <var-image lazy src="https://varlet.gitee.io/varlet-ui/cat.jpg" />
 </template>
 
-<script>
-import VarImage from '..'
-import AppType from '@varlet/cli/site/mobile/components/AppType'
-import VarRow from '../../row'
-import { watchLang } from '@varlet/cli/site/utils'
-import { use, pack } from './locale'
-
-export default {
-  name: 'ImageExample',
-  components: {
-    VarImage,
-    VarRow,
-    AppType,
-  },
-  setup() {
-    watchLang(use)
-
-    return { pack }
-  },
-}
-</script>
-
 <style scoped lang="less">
-.fit-item {
+.image-example-fit-item {
   color: #888;
   text-align: center;
   margin-right: 11px;
   margin-bottom: 11px;
 
-  div {
+  .image-example-margin-bottom {
     margin-bottom: 5px;
   }
 }
