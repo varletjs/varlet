b8721e2d5b8d545d3c98b69aed0cf564b2ab1e13
haoziqaq <357229046@qq.com>
2022-04-17 17:57:31 +0800

    docs(ui/layout): support script setup

--- packages/varlet-ui/src/row/example/Responsive.vue
+++ packages/varlet-ui/src/row/example/Responsive.vue
@@ -1,24 +1,16 @@
-<template>
-  <var-row :gutter="10">
-    <var-col class="col" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">1</var-col>
-    <var-col class="col" :xs="4" :sm="6" :md="8" :lg="9" :xl="11">2</var-col>
-    <var-col class="col" :xs="4" :sm="6" :md="8" :lg="9" :xl="11">3</var-col>
-    <var-col class="col" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">4</var-col>
-  </var-row>
-</template>
-
-<script>
-import { defineComponent } from 'vue'
+<script setup>
 import VarRow from '../../row'
 import VarCol from '../../col'
-
-export default defineComponent({
-  components: {
-    VarRow,
-    VarCol,
-  },
-})
 </script>
+
+<template>
+  <var-row :gutter="10">
+    <var-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">1</var-col>
+    <var-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">2</var-col>
+    <var-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">3</var-col>
+    <var-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">4</var-col>
+  </var-row>
+</template>
 
 <style scoped lang="less">
 .var-col {
