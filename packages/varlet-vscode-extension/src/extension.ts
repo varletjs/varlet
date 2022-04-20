16021e63c2c74ce4ee86f66f597751184e32432e
qytayh <13255238145@163.com>
2022-04-20 20:27:24 +0800

    chore(extension): varlet-vue2 site links switch to vercel #465

--- packages/varlet-vscode-extension/src/extension.ts
+++ packages/varlet-vscode-extension/src/extension.ts
@@ -3,9 +3,9 @@ import { componentMap, ComponentDescriptor } from './componentMap'
 import { bigCamelize, kebabCase } from './utils'
 
 const DOC = 'https://varlet-varletjs.vercel.app/#/zh-CN'
-const DOC_VUE2 = 'https://varlet.gitee.io/varlet-ui-vue2/#/zh-CN'
+const DOC_VUE2 = 'https://varlet-vue2.vercel.app/#/zh-CN'
 const EN_DOC = 'https://varlet-varletjs.vercel.app/#/en-US'
-const EN_DOC_VUE2 = 'https://varlet.gitee.io/varlet-ui-vue2/#/en-US'
+const EN_DOC_VUE2 = 'https://varlet-vue2.vercel.app/#/en-US'
 
 const LINK_RE = /(?<=<var-)([\w-]+)/g
 const BIG_CAMELIZE_RE = /(?<=<Var)([\w-]+)/g
