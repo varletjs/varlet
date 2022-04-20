b27e6e42b1a6aedc9989430d37d5c2674642c47d
songjianet <1778651752@qq.com>
2022-04-20 20:54:31 +0800

    test(cell): Update this cell component unit test.

--- packages/varlet-ui/src/cell/__tests__/index.spec.js
+++ packages/varlet-ui/src/cell/__tests__/index.spec.js
@@ -1,42 +1,162 @@
 import example from '../example'
 import Cell from '..'
 import VarCell from '../Cell'
+import VarIcon from '../../icon/Icon'
 import { mount } from '@vue/test-utils'
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 
-test('test cell example', () => {
-  const wrapper = mount(example)
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
-})
-
-test('test cell plugin', () => {
-  const app = createApp({}).use(Cell)
-  expect(app.component(Cell.name)).toBeTruthy()
-})
-
-test('test cell props', async () => {
-  const wrapper = mount(VarCell, {
-    props: {
-      title: 'This is Cell',
-      desc: 'This is desc',
-      border: true,
-      iconClass: 'test-icon-class',
-      titleClass: 'test-title-class',
-      descClass: 'test-desc-class',
-      extraClass: 'test-extra-class',
-      icon: 'fire',
-    },
-    slots: {
-      extra: () => 'text',
-    },
+describe('test cell component mount', () => {
+  test('test cell example', () => {
+    const wrapper = mount(example)
+    wrapper.unmount()
   })
-  expect(wrapper.find('.var-icon').exists()).toBe(true)
-  expect(wrapper.find('.var-icon').classes('var-icon-fire')).toBe(true)
-  expect(wrapper.classes('var-cell--border')).toBe(true)
-  expect(wrapper.find('.var-cell__title').text()).toBe('This is Cell')
-  expect(wrapper.find('.var-cell__desc').text()).toBe('This is desc')
-  expect(wrapper.find('.test-extra-class').text()).toBe('text')
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
+
+  test('test cell plugin', () => {
+    const app = createApp({}).use(Cell)
+    expect(app.component(Cell.name)).toBeTruthy()
+  })
+})
+
+describe('test cell component props', () => {
+  test('test cell title', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        title: 'This is cell',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__title').text()).toBe('This is cell')
+    wrapper.unmount()
+  })
+
+  test('test cell icon', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        icon: 'fire',
+      },
+    })
+
+    expect(wrapper.find('.var-icon').classes('var-icon-fire')).toBe(true)
+    wrapper.unmount()
+  })
+
+  test('test cell desc', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        desc: 'This is desc',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__desc').text()).toBe('This is desc')
+    wrapper.unmount()
+  })
+
+  test('test cell border', async () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        border: true,
+      },
+    })
+
+    expect(wrapper.classes('var-cell--border')).toBe(true)
+    await wrapper.setProps({ border: false })
+    expect(wrapper.classes('var-cell--border')).toBe(false)
+    wrapper.unmount()
+  })
+
+  test('test cell icon class', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        icon: 'fire',
+        iconClass: 'test-icon-class',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__icon').classes()).toContain('test-icon-class')
+    wrapper.unmount()
+  })
+
+  test('test cell title class', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        title: 'This is Cell',
+        titleClass: 'test-title-class',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__title').classes()).toContain('test-title-class')
+    wrapper.unmount()
+  })
+
+  test('test cell desc class', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        desc: 'This is desc',
+        descClass: 'test-desc-class',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__desc').classes()).toContain('test-desc-class')
+    wrapper.unmount()
+  })
+
+  test('test cell extra class', () => {
+    const wrapper = mount(VarCell, {
+      props: {
+        extraClass: 'test-extra-class',
+      },
+      slots: {
+        extra: 'information',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__extra').classes()).toContain('test-extra-class')
+    wrapper.unmount()
+  })
+})
+
+describe('test cell component slots', () => {
+  test('test cell default slots', () => {
+    const wrapper = mount(VarCell, {
+      slots: {
+        default: () => 'This is cell',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__title').element.textContent).toBe('This is cell')
+    wrapper.unmount()
+  })
+
+  test('test cell icon slots', () => {
+    const wrapper = mount(VarCell, {
+      slots: {
+        icon: h(VarIcon, { name: 'fire' }),
+      },
+    })
+
+    expect(wrapper.find('.var-icon').classes('var-icon-fire')).toBe(true)
+    wrapper.unmount()
+  })
+
+  test('test cell desc slots', () => {
+    const wrapper = mount(VarCell, {
+      slots: {
+        desc: () => 'This is desc',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__desc').text()).toBe('This is desc')
+    wrapper.unmount()
+  })
+
+  test('test cell extra slots', () => {
+    const wrapper = mount(VarCell, {
+      slots: {
+        extra: 'information',
+      },
+    })
+
+    expect(wrapper.find('.var-cell__extra').text()).toContain('information')
+    wrapper.unmount()
+  })
 })
