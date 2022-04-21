8cdd3559ca368cf1b9d5c04995ab87ae9227a474
songjianet <1778651752@qq.com>
2022-04-21 11:18:30 +0800

    test(icon): Update this icon component unit test.

--- packages/varlet-ui/src/icon/__tests__/index.spec.js
+++ packages/varlet-ui/src/icon/__tests__/index.spec.js
@@ -1,81 +1,97 @@
-import example from '../example'
 import Icon from '..'
 import VarIcon from '../Icon'
 import { mount } from '@vue/test-utils'
 import { createApp } from 'vue'
 import { delay } from '../../utils/jest'
 
-test('test icon example', () => {
-  const wrapper = mount(example)
-
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
+describe('test icon component use', () => {
+  test('test icon plugin', () => {
+    const app = createApp({}).use(Icon)
+    expect(app.component(Icon.name)).toBeTruthy()
+  })
 })
 
-test('test icon plugin', () => {
-  const app = createApp({}).use(Icon)
-  expect(app.component(Icon.name)).toBeTruthy()
+describe('test icon component event', () => {
+  test('test icon onClick', () => {
+    const onClick = jest.fn()
+    const wrapper = mount(VarIcon, {
+      props: {
+        onClick,
+      },
+    })
+
+    wrapper.trigger('click')
+    expect(onClick).toHaveBeenCalledTimes(1)
+    wrapper.unmount()
+  })
 })
 
-test('test icon onClick', () => {
-  const onClick = jest.fn()
+describe('test icon component props', () => {
+  test('test icon name', () => {
+    const wrapper = mount(VarIcon, {
+      props: {
+        name: 'checkbox-marked-circle',
+      },
+    })
 
-  const wrapper = mount(VarIcon, {
-    props: {
-      onClick,
-    },
+    expect(wrapper.find('.var-icon').classes()).toContain('var-icon-checkbox-marked-circle')
+    wrapper.unmount()
   })
 
-  wrapper.trigger('click')
-  expect(onClick).toHaveBeenCalledTimes(1)
-  wrapper.unmount()
-})
+  test('test icon size', async () => {
+    const wrapper = mount(VarIcon, {
+      props: {
+        size: '20px',
+      },
+    })
 
-test('test icon transition name', async () => {
-  const wrapper = mount(VarIcon, {
-    props: {
-      transition: 0,
-      name: 'check',
-    },
+    expect(wrapper.find('.var-icon').attributes('style')).toContain('font-size: 20px;')
+    await wrapper.setProps({ size: 30 })
+    expect(wrapper.find('.var-icon').attributes('style')).toContain('font-size: 30px;')
+    wrapper.unmount()
   })
 
-  expect(wrapper.find('.var-icon-check').exists()).toBeTruthy()
-  expect(wrapper.element.style.transition).toEqual('transform 0ms')
+  test('test icon color', () => {
+    const wrapper = mount(VarIcon, {
+      props: {
+        color: 'red',
+      },
+    })
 
-  await wrapper.setProps({
-    transition: 300,
-    name: 'error',
+    expect(wrapper.find('.var-icon').attributes('style')).toContain('color: red;')
+    wrapper.unmount()
   })
 
-  await delay(400)
+  test('test icon namespace', () => {
+    const wrapper = mount(VarIcon, {
+      props: {
+        namespace: 'my-icons',
+      },
+    })
 
-  expect(wrapper.find('.var-icon-error').exists()).toBeTruthy()
-  expect(wrapper.element.style.transition).toEqual('transform 300ms')
-  wrapper.unmount()
-})
-
-test('test icon namespace', () => {
-  const wrapper = mount(VarIcon, {
-    props: {
-      name: 'check',
-      namespace: 'custom-icon',
-    },
+    expect(wrapper.find('.var-icon').classes()).toContain('my-icons-undefined')
+    wrapper.unmount()
   })
 
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
-})
+  test('test icon transition', async () => {
+    const wrapper = mount(VarIcon, {
+      props: {
+        transition: 0,
+        name: 'check',
+      },
+    })
 
-test('test icon styles', () => {
-  const wrapper = mount(VarIcon, {
-    props: {
-      color: '#000',
-      size: '36px',
-      name: 'check',
-      transition: 0,
-    },
+    expect(wrapper.find('.var-icon-check').exists()).toBeTruthy()
+    expect(wrapper.element.style.transition).toEqual('transform 0ms')
+
+    await wrapper.setProps({
+      transition: 300,
+      name: 'error',
+    })
+    await delay(400)
+
+    expect(wrapper.find('.var-icon-error').exists()).toBeTruthy()
+    expect(wrapper.element.style.transition).toEqual('transform 300ms')
+    wrapper.unmount()
   })
-
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
 })
