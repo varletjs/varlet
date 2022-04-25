d2aa016b2511c561f63f837efca63525ea409600
EmberSpirit <85718018+wangKBweb@users.noreply.github.com>
2022-04-25 20:48:54 +0800

    fix(ui/picker): add picker missing style (#513)

--- packages/varlet-ui/src/themes/dark/picker.ts
+++ packages/varlet-ui/src/themes/dark/picker.ts
@@ -2,4 +2,6 @@ export default {
   '--picker-background': '#1e1e1e',
   '--picker-cancel-button-text-color': '#aaa',
   '--picker-picked-border': '1px solid rgba(255, 255, 255, 0.12)',
+  '--picker-mask-background-image':
+    'linear-gradient(180deg, hsla(0, 0%, 12%, 0.9), hsla(0, 0%, 12%, 0.4)), linear-gradient(0deg, hsla(0, 0%, 12%, 0.9), hsla(0, 0%, 12%, 0.4))',
 }
