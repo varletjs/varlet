5362f432d88809a361ff4ffaf9fe55d054bfec47
songjianet <1778651752@qq.com>
2022-04-26 21:51:41 +0800

    test(sticky): Update this sticky component unit test. (#516)

--- packages/varlet-ui/src/sticky/__tests__/index.spec.js
+++ packages/varlet-ui/src/sticky/__tests__/index.spec.js
@@ -1,164 +1,97 @@
 import Sticky from '..'
 import VarSticky from '../Sticky'
-import { delay, mockDoubleRaf, trigger } from '../../utils/jest'
+import { delay } from '../../utils/jest'
 import { mount } from '@vue/test-utils'
 import { createApp } from 'vue'
 
-mockDoubleRaf()
+const makeScroll = async (dom, name, offset) => {
+  const eventTarget = dom === document.documentElement ? window : dom
+  dom[name] = offset
+  eventTarget.dispatchEvent(
+    new CustomEvent('scroll', {
+      detail: {
+        target: {
+          [name]: offset,
+        },
+      },
+    })
+  )
 
-test('test sticky plugin', () => {
+  return await delay(100)
+}
+
+test('test sticky use', () => {
   const app = createApp({}).use(Sticky)
   expect(app.component(Sticky.name)).toBeTruthy()
 })
 
-test('test sticky z-index', async () => {
-  const wrapper = mount(VarSticky, {
-    props: {
-      zIndex: 100,
-    },
-    attachTo: document.body,
+describe('test sticky component props', () => {
+  test('test sticky offset-top', async () => {
+    const wrapper = mount(VarSticky, {
+      props: {
+        offsetTop: 100,
+      },
+      slots: {
+        default: () => 'This is default slots',
+      },
+      attachTo: document.body,
+    })
+
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).not.toContain('top: 100px;')
+    await makeScroll(document.documentElement, 'scrollTop', 200)
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).toContain('top: 100px;')
+    wrapper.unmount()
   })
-  await delay(100)
 
-  expect(wrapper.element.style.zIndex).toBe('100')
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
-})
+  test('test sticky z-index', async () => {
+    const wrapper = mount(VarSticky, {
+      props: {
+        zIndex: 200,
+      },
+      attachTo: document.body,
+    })
 
-test('test sticky onScroll', async () => {
-  const onScroll = jest.fn()
-
-  const wrapper = mount(VarSticky, {
-    props: {
-      onScroll,
-    },
-    slots: {
-      default: () => 'sticky content',
-    },
-    attachTo: document.body,
+    expect(wrapper.find('.var-sticky').attributes('style')).toContain('z-index: 200;')
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).toContain('z-index: 200;')
+    await makeScroll(document.documentElement, 'scrollTop', 0)
+    await wrapper.setProps({ zIndex: '100' })
+    expect(wrapper.find('.var-sticky').attributes('style')).toContain('z-index: 100;')
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).toContain('z-index: 100;')
+    wrapper.unmount()
   })
-  await delay(100)
 
-  expect(onScroll).toHaveBeenCalledTimes(1)
-  await trigger(window, 'scroll')
-  expect(onScroll).toHaveBeenCalledTimes(2)
-  wrapper.unmount()
-})
+  test('test sticky css-mode', async () => {
+    const wrapper = mount(VarSticky, {
+      props: {
+        cssMode: true,
+      },
+      attachTo: document.body,
+    })
 
-test('test sticky scrolling with css sticky position', async () => {
-  const mockGetComputedStyle = jest.spyOn(window, 'getComputedStyle').mockReturnValue({ position: 'sticky' })
-
-  const wrapper = mount(VarSticky, {
-    props: {
-      offsetTop: 100,
-      cssMode: true,
-    },
-    slots: {
-      default: () => 'sticky content',
-    },
-    attachTo: document.body,
+    expect(wrapper.find('.var-sticky--css-mode').exists()).toBe(true)
+    await makeScroll(document.documentElement, 'scrollTop', 0)
+    await wrapper.setProps({ cssMode: false })
+    expect(wrapper.find('.var-sticky--css-mode').exists()).toBe(false)
+    wrapper.unmount()
   })
-  await delay(100)
 
-  const mockGetBoundingClientRect = jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 200 })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
+  test('test sticky disabled', async () => {
+    const wrapper = mount(VarSticky, {
+      props: {
+        offsetTop: 100,
+        disabled: false,
+      },
+      slots: {
+        default: () => 'This is default slots',
+      },
+      attachTo: document.body,
+    })
 
-  jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({
-    top: 100,
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).not.toContain('top: 100px;')
+    await makeScroll(document.documentElement, 'scrollTop', 200)
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).toContain('top: 100px;')
+    await wrapper.setProps({ disabled: true })
+    expect(wrapper.find('.var-sticky__wrapper').attributes('style')).not.toContain('top: 100px;')
+    wrapper.unmount()
   })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  mockGetComputedStyle.mockRestore()
-  mockGetBoundingClientRect.mockRestore()
-  wrapper.unmount()
-})
-
-test('test sticky scrolling without css sticky position', async () => {
-  const mockGetComputedStyle = jest.spyOn(window, 'getComputedStyle').mockReturnValue({ position: 'relative' })
-
-  const wrapper = mount(VarSticky, {
-    props: {
-      offsetTop: 100,
-    },
-    slots: {
-      default: () => 'sticky content',
-    },
-    attachTo: document.body,
-  })
-  await delay(100)
-
-  const mockGetBoundingClientRect = jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 200 })
-
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 100 })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  mockGetComputedStyle.mockRestore()
-  mockGetBoundingClientRect.mockRestore()
-  wrapper.unmount()
-})
-
-test('test sticky disabled with css sticky position', async () => {
-  const mockGetComputedStyle = jest.spyOn(window, 'getComputedStyle').mockReturnValue({ position: 'sticky' })
-
-  const wrapper = mount(VarSticky, {
-    props: {
-      offsetTop: 100,
-      cssMode: true,
-      disabled: true,
-    },
-    slots: {
-      default: () => 'sticky content',
-    },
-    attachTo: document.body,
-  })
-  await delay(100)
-
-  const mockGetBoundingClientRect = jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 200 })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({
-    top: 100,
-  })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  mockGetComputedStyle.mockRestore()
-  mockGetBoundingClientRect.mockRestore()
-  wrapper.unmount()
-})
-
-test('test sticky disabled without css sticky position', async () => {
-  const mockGetComputedStyle = jest.spyOn(window, 'getComputedStyle').mockReturnValue({ position: 'relative' })
-
-  const wrapper = mount(VarSticky, {
-    props: {
-      offsetTop: 100,
-      disabled: true,
-    },
-    slots: {
-      default: () => 'sticky content',
-    },
-    attachTo: document.body,
-  })
-  await delay(100)
-
-  const mockGetBoundingClientRect = jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 200 })
-
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  jest.spyOn(wrapper.element, 'getBoundingClientRect').mockReturnValue({ top: 100 })
-  await trigger(window, 'scroll')
-  expect(wrapper.html()).toMatchSnapshot()
-
-  mockGetComputedStyle.mockRestore()
-  mockGetBoundingClientRect.mockRestore()
-  wrapper.unmount()
 })
