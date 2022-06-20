6ebcaf836f04808aeedc3aa04f4eebd811055e18
BeADre <beadre_wang@163.com>
2022-06-20 11:31:14 +0800

    chore: ensure @varlet/shared is built first

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,10 +1,10 @@
 import { buildCli, buildIcons, buildShared, buildUI, runTask } from './build.mjs'
 
 (async () => {
+  await runTask('shared', buildShared)
   await Promise.all([
     runTask('cli', buildCli),
-    runTask('icons', buildIcons),
-    runTask('shared', buildShared)
+    runTask('icons', buildIcons)
   ])
   await runTask('ui', () => buildUI(false))
 })()
