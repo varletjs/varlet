d1b243e90ad5dad50ad3da30231b0c89caf64f45
qytayh <13255238145@163.com>
2022-07-01 16:04:06 +0800

    fix(cli/site): support custom description

--- packages/varlet-cli/site/pc/pages/index/locale/en-US.ts
+++ packages/varlet-cli/site/pc/pages/index/locale/en-US.ts
@@ -1,5 +1,3 @@
 export default {
-  started: 'GET STARTED',
-  description: 'Varlet is a Material design mobile component library developed based on Vue3, developed and maintained by partners in the community. ' +
-    'Support Typescript, import on demand, dark mode, theme customization, internationalization, and provide VSCode plugin to ensure a good development experience',
+  started: 'GET STARTED'
 }
