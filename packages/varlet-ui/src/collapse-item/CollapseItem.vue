a48025bf7f945f94e2c07bcb9e1eab4e28495141
wegi8 <18272190626@163.com>
2022-07-18 10:56:05 +0800

    fix(collapse): ensure to trigger transitionend

--- packages/varlet-ui/src/collapse-item/CollapseItem.vue
+++ packages/varlet-ui/src/collapse-item/CollapseItem.vue
@@ -20,7 +20,7 @@
         </slot>
       </div>
     </div>
-    <div :class="n('content')" v-show="show" ref="contentEl" @transitionend="transitionend" @transitionstart="start">
+    <div :class="n('content')" v-show="show" ref="contentEl" @transitionend="transitionend">
       <div :class="n('content-wrap')">
         <slot />
       </div>
@@ -30,7 +30,7 @@
 
 <script lang="ts">
 import { defineComponent, ref, nextTick, watch, computed } from 'vue'
-import { nextTickFrame, requestAnimationFrame } from '../utils/elements'
+import { requestAnimationFrame } from '../utils/elements'
 import { isArray } from '@varlet/shared'
 import { createNamespace } from '../utils/components'
 import { useCollapse } from './provide'
@@ -50,7 +50,6 @@ export default defineComponent({
   setup(props) {
     const { index, collapse, bindCollapse } = useCollapse()
 
-    let isInitToTrigger = true // ensure to trigger transitionend
     const contentEl: Ref<HTMLDivElement | null> = ref(null)
     const show: Ref<boolean> = ref(false)
     const isShow: Ref<boolean> = ref(false)
@@ -75,27 +74,21 @@ export default defineComponent({
 
     const openPanel = () => {
       if (!contentEl.value) return
-      ;(contentEl.value as HTMLDivElement).style.height = ''
+        ;(contentEl.value as HTMLDivElement).style.height = ''
       show.value = true
 
       nextTick(() => {
         const { offsetHeight } = contentEl.value as HTMLDivElement
-        ;(contentEl.value as HTMLDivElement).style.height = 0 + 'px'
+          ;(contentEl.value as HTMLDivElement).style.height = 0 + 'px'
 
-        requestAnimationFrame(() => {
+        const raf = window.requestAnimationFrame
+          ? window.requestAnimationFrame
+          : setTimeout;
+
+        raf(() => {
           ;(contentEl.value as HTMLDivElement).style.height = offsetHeight + 'px'
-
-          if (!isInitToTrigger) return
-
-          nextTickFrame(() => {
-            if (isInitToTrigger) transitionend()
-          })
         })
       })
-    }
-
-    const start = () => {
-      isInitToTrigger = false
     }
 
     const closePanel = () => {
@@ -132,7 +125,6 @@ export default defineComponent({
 
     return {
       n,
-      start,
       classes,
       show,
       isShow,
