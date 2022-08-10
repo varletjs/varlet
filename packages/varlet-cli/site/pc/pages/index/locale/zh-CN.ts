51d45c1f92453632afd3252523c9d0c7db3a5017
BeADre <beadre_wang@163.com>
2022-08-10 16:20:53 +0800

    refactor(site): add indexpage to config

--- packages/varlet-cli/site/pc/pages/index/locale/zh-CN.ts
+++ packages/varlet-cli/site/pc/pages/index/locale/zh-CN.ts
@@ -1,5 +1,1 @@
-export default {
-  started: '起步',
-  description: 'Varlet 是一个基于 Vue3 开发的 Material 风格移动端组件库，' +
-    '全面拥抱 Vue3 生态，由社区团队维护。支持 Typescript、按需引入、暗黑模式、主题定制、国际化，并提供 VSCode 插件保障良好的开发体验',
-}
+// this file used to generate route path ('/zh-CN/index')
