ee7c3cd0c450ea1c0fd7f73a9234b1e1d6a32947
BeADre <beadre_wang@163.com>
2022-08-11 10:29:07 +0800

    refactor(cell)!: rename desc to description

--- packages/varlet-ui/src/cell/props.ts
+++ packages/varlet-ui/src/cell/props.ts
@@ -5,7 +5,7 @@ export const props = {
   icon: {
     type: String,
   },
-  desc: {
+  description: {
     type: String,
   },
   border: {
@@ -18,7 +18,7 @@ export const props = {
   titleClass: {
     type: String,
   },
-  descClass: {
+  descriptionClass: {
     type: String,
   },
   extraClass: {
