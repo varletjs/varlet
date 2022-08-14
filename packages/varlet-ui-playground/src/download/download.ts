9d8e4f1aaeb7fe77ab207fcb62573cf44a273889
CharlesChanV <charleschan2015@hotmail.com>
2022-08-14 16:38:27 +0800

    chore: fix the program exclude 'Playground.vue' from downloaded files

--- packages/varlet-ui-playground/src/download/download.ts
+++ packages/varlet-ui-playground/src/download/download.ts
@@ -6,7 +6,7 @@ import pkg from './template/package.json?raw'
 import config from './template/vite.config.js?raw'
 import readme from './template/README.md?raw'
 
-const excludes = ['varlet-repl-plugin.js', 'import-map.json']
+const excludes = ['varlet-repl-plugin.js', 'import-map.json', 'Playground.vue']
 
 export async function downloadProject(store: any) {
   // eslint-disable-next-line no-alert
@@ -31,13 +31,13 @@ export async function downloadProject(store: any) {
   // eslint-disable-next-line guard-for-in,no-restricted-syntax
   for (const file in files) {
     if (!excludes.includes(file)) {
-      let code = files[file]
+      const code = files[file]
 
-      if (file === 'App.vue') {
-        code = code
-          .replace("import { installVarletUI } from './varlet-repl-plugin.js'\n", '')
-          .replace('installVarletUI()\n', '')
-      }
+      // if (file === 'App.vue') {
+      //   code = code
+      //     .replace("import { installVarletUI } from './varlet-repl-plugin.js'\n", '')
+      //     .replace('installVarletUI()\n', '')
+      // }
 
       src.file(file, code)
     }
