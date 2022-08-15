aed33dad220cf9888198a373d5efeaaeff468665
erkelost <1256029807@qq.com>
2022-08-15 11:52:39 +0800

    fix(depoly): package version cause Deployment fail

--- packages/varlet-cli/src/index.ts
+++ packages/varlet-cli/src/index.ts
@@ -38,10 +38,10 @@ program.command('lint').description('Lint code').action(lint)
 program
   .command('create')
   .description('Create a component directory')
-  .option('-s, --sfc', 'Disable to generator i18n files')
-  .option('-t, --tsx', 'Disable to generator i18n files')
-  .option('-j, --jsx', 'Disable to generator i18n files')
-  .option('-l, --locale', 'Disable to generator internationalized files')
+  .option('-s, --sfc', 'Generate files in sfc format')
+  .option('-t, --tsx', 'Generate files in tsx format')
+  .option('-j, --jsx', 'Generate files in jsx format')
+  .option('-l, --locale', 'Generator internationalized files')
   .action(create)
 
 program
