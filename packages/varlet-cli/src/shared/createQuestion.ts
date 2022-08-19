b02685c21a2979e770784bce4af6af97d130cc15
ERKELSOT <66500121+ErKeLost@users.noreply.github.com>
2022-08-19 14:34:29 +0800

    refactor(cli): refactor create command (#673)

---
+++ packages/varlet-cli/src/shared/createQuestion.ts
@@ -0,0 +1,11 @@
+import prompts from 'prompts'
+
+export default async function createQuestion(question: any, options: any) {
+  const result = await prompts(question, {
+    onCancel: () => {
+      throw new Error('❌ Operation cancelled')
+    },
+  })
+  Object.assign(options, result)
+  return Promise.resolve(options)
+}
---
+++ packages/varlet-cli/template/create/props.ts
@@ -0,0 +1,8 @@
+import type { PropType } from 'vue'
+
+export const props = {
+  type: {
+    type: String as PropType<'default' | 'primary' | 'info' | 'success' | 'warning' | 'danger'>,
+    default: 'default',
+  },
+}
