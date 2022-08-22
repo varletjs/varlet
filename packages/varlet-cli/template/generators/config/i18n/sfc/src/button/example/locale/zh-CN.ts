c661a3cf88290d754f9e7dd80de4a4d89fd2ebc9
BeADre <beadre_wang@163.com>
2022-08-22 14:36:46 +0800

    refactor(cli/gen): move generators folder to template folder

---
+++ packages/varlet-cli/template/generators/config/i18n/sfc/src/button/example/locale/zh-CN.ts
@@ -0,0 +1,5 @@
+export default {
+  basicUse: '基本使用',
+  modifyColor: '修改颜色',
+  start: '起步',
+}
---
+++ packages/varlet-cli/template/generators/config/i18n/tsx/src/button/Button.tsx
@@ -0,0 +1,33 @@
+import { defineComponent, PropType } from 'vue'
+import './button.less'
+
+// i18n for component's internal
+// import { pack } from '../locale'
+
+export default defineComponent({
+  name: 'BButton',
+  props: {
+    color: {
+      type: String,
+    },
+    onClick: {
+      type: Function as PropType<(e: Event) => void>,
+    },
+  },
+  setup(props, { slots }) {
+    const handleClick = (e: Event) => props.onClick?.(e)
+
+    return () => {
+      const { color } = props
+
+      return (
+        <>
+          <button class="basic-button" style={{ background: color }} onClick={handleClick}>
+            {/* { pack.value.button } */}
+            {slots.default?.()}
+          </button>
+        </>
+      )
+    }
+  },
+})
