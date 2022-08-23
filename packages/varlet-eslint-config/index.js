cf261e6b6c27812bf154ed3c82734fb8289e27bd
BeADre <beadre_wang@163.com>
2022-08-23 15:01:47 +0800

    fix(playground): remove import about vite and fix ts error

--- packages/varlet-eslint-config/index.js
+++ packages/varlet-eslint-config/index.js
@@ -52,6 +52,7 @@ module.exports = {
     'vue/require-explicit-emits': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-extra-semi': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
