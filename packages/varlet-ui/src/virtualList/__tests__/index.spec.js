7a33928a94c214219873ef42a71ca0dc16a6ace9
tomato <773577089@qq.com>
2022-09-11 10:43:12 +0800

    fix: 删除无用组件

--- packages/varlet-ui/src/virtualList/__tests__/index.spec.js
+++
@@ -1,8 +0,0 @@
-import VirtualList from '..'
-import { createApp } from 'vue'
-import { mount } from '@vue/test-utils'
-
-test('test virtualList use', () => {
-  const app = createApp({}).use(VirtualList)
-  expect(app.component(VirtualList.name)).toBeTruthy()
-})
