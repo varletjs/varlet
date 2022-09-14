8d22800573bd2795deb345933b50af0df872523b
haoziqaq <357229046@qq.com>
2022-09-14 23:08:27 +0800

    refactor(cli/site): optimize index page css

--- packages/varlet-cli/site/pc/components/AppMobile.vue
+++ packages/varlet-cli/site/pc/components/AppMobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="varlet-site-mobile">
+  <div class="varlet-site-mobile varlet-site-mobile--375">
     <div class="varlet-site-mobile-content">
       <iframe id="mobile" :src="`./mobile.html#/${componentName}?language=${language}&platform=pc&replace=${replace}`"></iframe>
     </div>
@@ -26,13 +26,19 @@ export default {
 <style scoped lang="less">
 .varlet-site-mobile {
   position: sticky;
-  flex: 0 0 350px;
+  flex: 0 0 330px;
   top: 80px;
   height: calc(100vh - 100px);
   margin-right: 38px;
   overflow: hidden;
   box-shadow: 0 0 14px 6px var(--site-config-color-shadow);
   border-radius: 2px;
+
+  @media screen and (min-width: 1600px) {
+    &--375 {
+      flex: 0 0 375px;
+    }
+  }
 
   &-content {
     width: 100%;
