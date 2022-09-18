58239e65a1c3ad75735572601e2447c7ee151ca1
raindemimi <2271173295@qq.com>
2022-09-18 17:20:51 +0800

    docs: update information in the documentation docs(ui/menu): fix a documentation error in the ui/menu scope

---
+++ packages/varlet-ui/src/menu/example/locale/zh-CN.ts
@@ -0,0 +1,15 @@
+export default {
+  alignmentMethods: '对齐方式',
+  topAlignment: '顶部对齐',
+  menuOption: '菜单项',
+  bottomAlignment: '底部对齐',
+  offset: '偏移量',
+  offsetRight: '右偏移',
+  offsetLeft: '左偏移',
+  offsetBottom: '下偏移',
+  offsetTop: '上偏移',
+  events: '注册事件',
+  disabled: '禁用菜单',
+  placement: '菜单位置',
+  trigger: '触发方式',
+}
