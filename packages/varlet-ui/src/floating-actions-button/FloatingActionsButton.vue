72c24bf03f20c83d9dafbf298f1fa94a71199550
luokavin <luokavin@foxmail.com>
2022-10-26 17:36:33 +0800

    feat: add floating-actions-button props

--- packages/varlet-ui/src/floating-actions-button/FloatingActionsButton.vue
+++ packages/varlet-ui/src/floating-actions-button/FloatingActionsButton.vue
@@ -1,18 +1,46 @@
 <template>
-  <div :class="n()"></div>
+  <div :class="n()">
+    <var-button :class="fabLocation" :disabled="disabled" round>
+      <var-icon :name="inactiveIcon" />
+    </var-button>
+  </div>
 </template>
 
 <script lang="ts">
+// import VarButton from "../button";
+// import VarIcon from "../icon";
 import { props } from './props'
-import { createNamespace } from '../utils/components'
-import { defineComponent } from 'vue'
+import { call, createNamespace } from '../utils/components'
+import { defineComponent, ref, Ref, watch } from 'vue'
 
 const { n } = createNamespace('floating-actions-button')
 
 export default defineComponent({
   name: 'VarFloatingActionsButton',
   props,
-  setup() {
+  setup(props) {
+    const show: Ref<boolean> = ref(false)
+
+    // expose
+    // const open = () => {
+    //   const { disabled } = props;
+    //
+    //   if (disabled) {
+    //     return;
+    //   }
+    //
+    //   show.value = true;
+    //   call(props["onUpdate:show"], true);
+    // };
+
+    // expose
+    const close = () => {
+      show.value = false
+      call(props['onUpdate:show'], false)
+    }
+
+    watch(() => props.disabled, close)
+
     return {
       n,
     }
--- packages/varlet-ui/src/floating-actions-button/props.ts
+++ packages/varlet-ui/src/floating-actions-button/props.ts
@@ -1,1 +1,81 @@
-export const props = {}
+import type { PropType } from 'vue'
+
+export type Trigger = 'click' | 'hover'
+
+export type FabLocation = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+
+export type ActionsLocation = 'top' | 'bottom' | 'left' | 'right'
+
+export type Action = {
+  icon?: string
+  iconColor?: string
+  iconSize?: string | number
+  iconNamespace?: string
+  buttonColor?: string
+  buttonSize?: string | number
+}
+
+export const props = {
+  // 是否显示面板
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  'onUpdate:show': {
+    type: Function as PropType<(show: boolean) => void>,
+  },
+  // 是否禁用 fab
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  // 菜单触发方式
+  trigger: {
+    type: String as PropType<Trigger>,
+    default: 'click',
+  },
+  // fab 定位
+  fabLocation: {
+    type: String as PropType<FabLocation>,
+    default: 'bottom-right',
+  },
+  // fab 颜色
+  fabColor: {
+    type: String,
+    default: '#3a7afe',
+  },
+  // fab 尺寸
+  fabIconSize: {
+    type: String,
+    default: '',
+  },
+  // actions 定位
+  actionsLocation: {
+    type: String as PropType<ActionsLocation>,
+    default: 'top',
+  },
+  actions: {
+    type: Array as PropType<Array<Action>>,
+    default: [],
+  },
+  inactiveIcon: {
+    type: String,
+    default: 'plus',
+  },
+  activeIcon: {
+    type: String,
+    default: 'window-close',
+  },
+  overlay: {
+    type: Boolean,
+    default: false,
+  },
+  overlayClass: {
+    type: String,
+    default: '',
+  },
+  overlayStyle: {
+    type: Object as PropType<Record<string, unknown>>,
+    default: {},
+  },
+}
