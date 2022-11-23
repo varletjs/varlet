c1fd41f059c98cb939642ac28f739ff93f7eef8b
qytayh <13255238145@163.com>
2022-11-23 22:17:30 +0800

    refactor(ui/result): fix css

--- packages/varlet-ui/src/result/example/index.vue
+++ packages/varlet-ui/src/result/example/index.vue
@@ -3,6 +3,16 @@ import VarResult from '../index'
 import VarButton from '../../button'
 import { watchLang, AppType } from '@varlet/cli/client'
 import { use, pack } from './locale'
+import { ref } from 'vue'
+
+const showSuccessResult = ref(true)
+
+const handleClickSuccess = () => {
+  showSuccessResult.value = false
+  setTimeout(() => {
+    showSuccessResult.value = true
+  })
+}
 
 watchLang(use)
 </script>
@@ -10,12 +20,13 @@ watchLang(use)
 <template>
   <app-type>Mobile phone example code</app-type>
   <var-result
+    v-if="showSuccessResult"
     status="success"
     description="descriptiondescriptiondescriptiondescriptiondescriptiondescription"
     title="title"
   >
     <template #footer>
-      <var-button type="success">success</var-button>
+      <var-button type="success" @click="handleClickSuccess">success</var-button>
     </template>
   </var-result>
 </template>
