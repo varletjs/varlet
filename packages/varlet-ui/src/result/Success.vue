06a0ffb91694fceb900cff628a584c21b271fcc4
qytayh <13255238145@163.com>
2022-11-23 17:23:51 +0800

    refactor(ui/result): add success component

---
+++ packages/varlet-ui/src/result/Success.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="n()">
+    <span :class="classes(n('--line'), n('--line-long'))" />
+    <span :class="classes(n('--line'), n('--line-tip'))" />
+    <span :class="n('--circle')" />
+    <span :class="n('--fix')" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { createNamespace } from '../utils/components'
+import { toSizeUnit } from '../utils/elements'
+
+const { n, classes } = createNamespace('result__success')
+
+export default defineComponent({
+  name: 'VarResult',
+  props: {
+    imageSize: {
+      type: [String, Number],
+    },
+  },
+  setup(props) {
+    return {
+      n,
+      classes,
+      toSizeUnit,
+    }
+  },
+})
+</script>
+
+<style lang="less">
+@import './result.less';
+</style>
