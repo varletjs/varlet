d7a1995323c8669339a447be3662108474cc3ff9
sdhushu <1426732052@qq.com>
2022-11-26 12:57:28 +0800

    docs: update zh-CN md

--- packages/varlet-ui/src/overlay/example/local/en-US.ts
+++ packages/varlet-ui/src/overlay/example/local/en-US.ts
@@ -7,4 +7,5 @@ export default {
   overlayStyle: 'overlay style',
   event: 'event',
   clickOverlay: 'clickOverlay',
+  text: 'As he came into the window. It was the sound of a crescendo. He came into her apartment. He left the bloodstains on the carpet.',
 }
