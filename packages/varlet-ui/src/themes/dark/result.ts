1b0d5a5742da3cdf7dd419327148af481ecfffee
qytayh <13255238145@163.com>
2022-11-30 23:31:00 +0800

    docs(ui/card): opt slot example

--- packages/varlet-ui/src/themes/dark/result.ts
+++ packages/varlet-ui/src/themes/dark/result.ts
@@ -2,4 +2,8 @@ export default {
   '--result-background': '#303030',
   '--result-title-color': '#ffffff',
   '--result-description-color': '#aaaaaa',
+  '--result-question-color': '#89cef0',
+  '--result-question-border-color': '#97c2d84d',
+  '--result-empty-color': '#e0e0e0',
+  '--result-empty-border-color': '#e8e5e54d',
 }
