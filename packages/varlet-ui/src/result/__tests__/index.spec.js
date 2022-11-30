f02e4207d572e0c884e3a5881e3470b0bb04ab7e
qytayh <13255238145@163.com>
2022-11-30 21:43:14 +0800

    test(ui/result): add test cases

--- packages/varlet-ui/src/result/__tests__/index.spec.js
+++ packages/varlet-ui/src/result/__tests__/index.spec.js
@@ -1,4 +1,5 @@
 import Result from '..'
+import VarResult from '../Result'
 import { createApp } from 'vue'
 import { mount } from '@vue/test-utils'
 
@@ -10,4 +11,49 @@ test('test result plugin', () => {
 test('test result snapshot', () => {
   const wrapper = mount(Result)
   expect(wrapper.html()).toMatchSnapshot()
+})
+
+describe('test result component props', () => {
+  test('test result type', () => {
+    ;['info', 'success', 'warning', 'error', 'question', 'empty'].forEach((type) => {
+      const wrapper = mount(VarResult, {
+        props: { type },
+      })
+
+      expect(wrapper.find('.var-result__image').classes()).toContain('var-result__' + type)
+      wrapper.unmount()
+    })
+  })
+
+  test('test result imageSize', () => {
+    const wrapper = mount(VarResult, {
+      props: {
+        type: 'empty',
+        imageSize: '100px',
+      },
+    })
+
+    expect(wrapper.find('.var-result__empty').attributes('style')).toContain('width: calc(100px * 0.9)')
+    expect(wrapper.find('.var-result__empty').attributes('style')).toContain('height: calc(100px * 0.9)')
+
+    wrapper.unmount()
+  })
+
+  test('test result title', () => {
+    const wrapper = mount(VarResult, {
+      props: { title: 'This is title' },
+    })
+
+    expect(wrapper.find('.var-result__title').element.textContent).toBe('This is title')
+    wrapper.unmount()
+  })
+
+  test('test result description', () => {
+    const wrapper = mount(VarResult, {
+      props: { description: 'This is description' },
+    })
+
+    expect(wrapper.find('.var-result__description').element.textContent).toBe('This is description')
+    wrapper.unmount()
+  })
 })
