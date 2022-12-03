e14330415764d0bfc04ba92f4940131220de52cd
sdhushu <1426732052@qq.com>
2022-12-03 15:13:42 +0800

    fix: overlay code review

--- packages/varlet-ui/src/overlay/Overlay.tsx
+++ packages/varlet-ui/src/overlay/Overlay.tsx
@@ -1,15 +1,11 @@
-import { defineComponent, Teleport, useAttrs } from 'vue'
+import { defineComponent, Teleport, useAttrs, Transition } from 'vue'
 import { props } from './props'
+import { useLock } from '../context/lock'
 import { useZIndex } from '../context/zIndex'
 import { createNamespace, useTeleport } from '../utils/components'
 
 import '../styles/common.less'
 import './overlay.less'
-
-interface attrsAttribute {
-  class?: string
-  style?: StyleSheet
-}
 
 const { n, classes } = createNamespace('overlay')
 
@@ -20,40 +16,47 @@ export default defineComponent({
   setup(props, { slots }) {
     const { zIndex } = useZIndex(() => props.show, 1)
     const { disabled } = useTeleport()
-    const attrs: attrsAttribute = useAttrs()
+    const attrs = useAttrs()
     const hideOverlay = () => {
-      const { onClickOverlay } = props
-      onClickOverlay?.()
+      const { onClick } = props
+      onClick?.()
       props['onUpdate:show']?.(false)
     }
 
+    useLock(
+      () => props.show,
+      () => props.lockScroll
+    )
+
     const renderOverlay = () => {
+      return (
+        <div
+          class={classes(n())}
+          style={{
+            zIndex: zIndex.value - 1,
+          }}
+          {...attrs}
+          onClick={hideOverlay}
+        >
+          {slots.default?.()}
+        </div>
+      )
+    }
+
+    const renderTransitionOverlay = () => {
       const { show } = props
-      if (show) {
-        return (
-          <div
-            class={classes(n(), attrs.class)}
-            style={{
-              zIndex: zIndex.value - 1,
-              ...attrs.style,
-            }}
-            onClick={hideOverlay}
-          >
-            {slots.default?.()}
-          </div>
-        )
-      }
+      return <Transition name={n('$-fade')}>{show && renderOverlay()}</Transition>
     }
     return () => {
       const { teleport } = props
       if (teleport) {
         return (
           <Teleport to={teleport} disabled={disabled.value}>
-            {renderOverlay()}
+            {renderTransitionOverlay()}
           </Teleport>
         )
       }
-      return renderOverlay()
+      return renderTransitionOverlay()
     }
   },
 })
