d5a9b0e6f0c78036290fa8164e75e39dc8e8e9cf
BeADre <beadre_wang@163.com>
2022-12-09 16:24:32 +0800

    fix(site): replace progress to loading-bar

---
+++ packages/varlet-cli/site/components/loading-bar/props.ts
@@ -0,0 +1,23 @@
+export const props = {
+  value: {
+    type: Number,
+    default: 0,
+  },
+  opacity: {
+    type: Number,
+    default: 0,
+  },
+  error: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: String,
+  },
+  errorColor: {
+    type: String,
+  },
+  height: {
+    type: [Number, String],
+  },
+}
