102b80001cab93e423d4f92fef203f6f920874dc
linkscope <linkscope@foxmail.com>
2022-12-18 23:32:43 +0800

    docs(ui/breadcrumb): optimize docs

--- packages/varlet-ui/src/breadcrumb/example/index.vue
+++ packages/varlet-ui/src/breadcrumb/example/index.vue
@@ -2,20 +2,55 @@
 import { watchLang, AppType } from '@varlet/cli/client'
 import VarBreadcrumb from '..'
 import VarBreadcrumbItem from '../../breadcrumb-item'
+import VarSpace from '../../space'
+import VarIcon from '../../icon'
 import { use, pack } from './locale'
 
 watchLang(use)
 </script>
 
 <template>
-  <app-type>Mobile phone example code</app-type>
+  <app-type>{{ pack.separator }}</app-type>
+  <var-space direction="column" size="large">
+    <var-breadcrumb>
+      <var-breadcrumb-item>{{ pack.level1 }}</var-breadcrumb-item>
+      <var-breadcrumb-item>{{ pack.level2 }}</var-breadcrumb-item>
+      <var-breadcrumb-item>{{ pack.level3 }}</var-breadcrumb-item>
+    </var-breadcrumb>
+
+    <var-breadcrumb separator="\">
+      <var-breadcrumb-item>{{ pack.level1 }}</var-breadcrumb-item>
+      <var-breadcrumb-item>{{ pack.level2 }}</var-breadcrumb-item>
+      <var-breadcrumb-item>{{ pack.level3 }}</var-breadcrumb-item>
+    </var-breadcrumb>
+  </var-space>
+
+  <app-type>{{ pack.levelSeparator }}</app-type>
   <var-breadcrumb>
-    <var-breadcrumb-item>一级</var-breadcrumb-item>
-    <var-breadcrumb-item separator="->">二级</var-breadcrumb-item>
+    <var-breadcrumb-item>{{ pack.level1 }}</var-breadcrumb-item>
+    <var-breadcrumb-item separator="~">{{ pack.level2 }}</var-breadcrumb-item>
+    <var-breadcrumb-item>{{ pack.level3 }}</var-breadcrumb-item>
+  </var-breadcrumb>
+
+  <app-type>{{ pack.slotSeparator }}</app-type>
+  <var-breadcrumb>
     <var-breadcrumb-item>
-      <template #separator> 分割符 </template>
-      三级
+      <template #separator>
+        <var-icon name="chevron-right" />
+      </template>
+      {{ pack.level1 }}
     </var-breadcrumb-item>
-    <var-breadcrumb-item>四级</var-breadcrumb-item>
+    <var-breadcrumb-item>
+      <template #separator>
+        <var-icon name="chevron-right" />
+      </template>
+      {{ pack.level2 }}
+    </var-breadcrumb-item>
+    <var-breadcrumb-item>
+      <template #separator>
+        <var-icon name="chevron-right" />
+      </template>
+      {{ pack.level3 }}
+    </var-breadcrumb-item>
   </var-breadcrumb>
 </template>
