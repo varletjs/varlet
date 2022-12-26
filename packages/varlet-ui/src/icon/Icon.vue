c7aef505c95689998d5c0765dc9b4a62fc2d681a
haoziqaq <357229046@qq.com>
2022-12-27 00:56:46 +0800

    refactor(ui/icon): remove unuseful namespace class mounted

--- packages/varlet-ui/src/icon/Icon.vue
+++ packages/varlet-ui/src/icon/Icon.vue
@@ -4,7 +4,7 @@
     :class="
       classes(
         n(),
-        namespace,
+        [namespace !== n(), namespace],
         `${namespace}--set`,
         [isURL(name), n('image'), `${namespace}-${nextName}`],
         [shrinking, n('--shrinking')]
