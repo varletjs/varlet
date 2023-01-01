2736ca47f47a7552d1519bfd233419e00b29c0fa
ERKELSOT <1256029807@qq.com>
2023-01-01 10:58:21 +0800

    fix: color picker composable

--- packages/varlet-ui/src/color-picker/utils/composable.ts
+++ packages/varlet-ui/src/color-picker/utils/composable.ts
@@ -1,26 +1,6 @@
 import { Ref, ref, watch } from 'vue'
 import { ColorPickerColor, CssColorObject } from './color-utils-types'
 
-export function colorPickerResize(colorCubeRef: Ref<HTMLElement | null>, top: Ref<number>, left: Ref<number>): void {
-  if (colorCubeRef.value) {
-    const rect = colorCubeRef.value.getBoundingClientRect()
-    left.value = rect.left
-    top.value = rect.top + window.scrollY + rect.height
-  }
-}
-export function isExhibitionColorPicker(
-  event: PointerEvent,
-  colorCubeRef: Ref<HTMLElement | null>,
-  pickerRef: Ref<HTMLElement | null>,
-  showColorPicker: Ref<boolean>
-): void {
-  if (colorCubeRef.value?.contains?.(event.target as Node) && !showColorPicker.value) {
-    showColorPicker.value = true
-  }
-  if (!!pickerRef.value && !pickerRef.value?.contains(event.target as Node)) {
-    showColorPicker.value = !showColorPicker.value
-  }
-}
 export function useReactive<T>(source: () => T): Ref<T | undefined> {
   const model = ref<T>()
   model.value = source()
@@ -36,7 +16,6 @@ export function changeColorValue(value: ColorPickerColor, maxValue: number): Css
 
   if (value.alpha > maxValue) {
     return value.hsva.v > maxValue && value.hsva.s < maxValue ? { color: '#000' } : { color: '#fff' }
-  } else {
-    return { color: '#000' }
   }
+  return { color: '#000' }
 }
