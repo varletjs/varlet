76555b16ab5c0db832d308430a49dfbd567a5744
sdhushu <1426732052@qq.com>
2023-02-12 22:17:19 +0800

    feat(button-group): add button group test

--- packages/varlet-ui/src/button-group/__tests__/index.spec.js
+++ packages/varlet-ui/src/button-group/__tests__/index.spec.js
@@ -6,3 +6,26 @@ test('test button-group plugin', () => {
   const app = createApp({}).use(ButtonGroup)
   expect(app.component(ButtonGroup.name)).toBeTruthy()
 })
+
+describe('test button-group component props', () => {
+  test('test button-group mode', () => {
+    const wrapper = mount(ButtonGroup, {
+      props: {
+        mode: 'outline',
+        vertical: false,
+      },
+    })
+    expect(wrapper.classes()).toContain('var-button-group' + '--mode')
+    wrapper.unmount()
+  })
+
+  test('test button-group vertical', () => {
+    const wrapper = mount(ButtonGroup, {
+      props: {
+        vertical: true,
+      },
+    })
+    expect(wrapper.classes()).toContain('var-button-group' + '--vertical')
+    wrapper.unmount()
+  })
+})
