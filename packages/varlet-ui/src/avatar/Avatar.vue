929ca4b1e98bb997a05792eaf6b0fef6f34f6b6b
ayang <473033518@qq.com>
2023-02-15 16:13:35 +0800

    feat(ui): added avatar component

---
+++ packages/varlet-ui/src/avatar/Avatar.vue
@@ -0,0 +1,134 @@
+<template>
+  <div
+    ref="avatarElement"
+    :class="
+      classes(n(), [internalSizeValidator(size), n(`--${size}`)], [round, n('--round')], [bordered, n('--bordered')])
+    "
+    :style="{
+      width: !internalSizeValidator(size) ? toSizeUnit(size) : undefined,
+      height: !internalSizeValidator(size) ? toSizeUnit(size) : undefined,
+      borderColor,
+      backgroundColor: color,
+    }"
+  >
+    <template v-if="src">
+      <img
+        :class="n('image')"
+        :src="src"
+        :style="{ objectFit: fit }"
+        v-if="lazy"
+        v-lazy="src"
+        :lazy-loading="loading"
+        :lazy-error="loading"
+        @load="handleLoad"
+        @error="handleError"
+        @click="handleClick"
+      />
+
+      <img
+        :class="n('image')"
+        :src="src"
+        :style="{ objectFit: fit }"
+        v-else
+        @load="handleLoad"
+        @error="handleError"
+        @click="handleClick"
+      />
+    </template>
+
+    <div
+      ref="textElement"
+      :class="n('text')"
+      :style="{
+        scale,
+      }"
+      v-else
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, onUpdated } from 'vue'
+import { props, internalSizeValidator, sizeValidator } from './props'
+import { toSizeUnit } from '../utils/elements'
+import { createNamespace, call } from '../utils/components'
+import type { Ref } from 'vue'
+import type { LazyHTMLElement } from '../lazy'
+
+const { n, classes } = createNamespace('avatar')
+
+export default defineComponent({
+  name: 'VarAvatar',
+  components: {},
+  props,
+  setup(props) {
+    const avatarElement: Ref<HTMLDivElement | null> = ref(null)
+    const textElement: Ref<HTMLDivElement | null> = ref(null)
+
+    const scale: Ref<number> = ref(1)
+
+    const getScale = () => {
+      if (!avatarElement.value || !textElement.value) {
+        scale.value = 1
+        return
+      }
+
+      const avatarElementWidth = avatarElement.value.offsetWidth
+      const textElementWidth = textElement.value.offsetWidth
+
+      if (avatarElementWidth > textElementWidth) {
+        scale.value = 1
+      } else {
+        scale.value = avatarElementWidth / textElementWidth
+      }
+    }
+
+    const handleLoad = (e: Event) => {
+      const el: LazyHTMLElement = e.currentTarget as LazyHTMLElement
+      const { lazy, onLoad, onError } = props
+
+      if (lazy) {
+        el._lazy.state === 'success' && call(onLoad, e)
+        el._lazy.state === 'error' && call(onError, e)
+      } else {
+        call(onLoad, e)
+      }
+    }
+
+    const handleError = (e: Event) => {
+      const { lazy, onError } = props
+
+      !lazy && call(onError, e)
+    }
+
+    const handleClick = (e: Event) => {
+      call(props.onClick, e)
+    }
+
+    onMounted(getScale)
+
+    onUpdated(getScale)
+
+    return {
+      internalSizeValidator,
+      sizeValidator,
+      toSizeUnit,
+      n,
+      classes,
+      avatarElement,
+      textElement,
+      scale,
+      handleLoad,
+      handleError,
+      handleClick,
+    }
+  },
+})
+</script>
+
+<style lang="less">
+@import '../styles/common';
+@import './avatar.less';
+</style>
