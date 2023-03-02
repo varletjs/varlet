130bd4a192f07b1d05f7bbf9b289ac0ef0ca94aa
forestxieCode <1397798719@qq.com>
2023-03-03 01:07:05 +0800

    fix(ui/fab): remove redundant props

--- packages/varlet-ui/types/fab.d.ts
+++ packages/varlet-ui/types/fab.d.ts
@@ -1,13 +1,12 @@
 import { VNode } from 'vue'
 import { VarComponent, BasicAttributes } from './varComponent'
-import type { Transition, Trigger, Direction, Position } from '../src/fab/props'
+import type { Trigger, Direction, Position } from '../src/fab/props'
 
 export declare const fabProps: Record<string, any>
 
 export interface FabProps extends BasicAttributes {
   trigger?: Trigger
   direction?: Direction
-  transition?: Transition
   position?: Position
 }
 
