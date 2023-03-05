ed9aac8b434e59ba9b7ffa9be1c7adf2273a11d3
forestxieCode <1397798719@qq.com>
2023-03-05 20:20:45 +0800

    feat(ui/fab): updating types

--- packages/varlet-ui/types/fab.d.ts
+++ packages/varlet-ui/types/fab.d.ts
@@ -4,10 +4,16 @@ import { VarComponent, BasicAttributes, ListenerProp } from './varComponent'
 export type FabPosition = 'left-top' | 'right-top' | 'left-bottom' | 'right-bottom'
 export type FabTrigger = 'click' | 'hover'
 export type FabDirection = 'top' | 'right' | 'bottom' | 'left'
+export type Type = 'default' | 'primary' | 'info' | 'success' | 'warning' | 'danger'
 
 export declare const fabProps: Record<string, any>
 export interface FabProps extends BasicAttributes {
   modelValue?: boolean
+  color?: string
+  textColor?: string
+  icon?: string
+  iconSize?: number | string
+  type: Type
   trigger?: FabTrigger
   direction?: FabDirection
   position?: FabPosition
