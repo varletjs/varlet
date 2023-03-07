fd69f23206593ba2fa7748d4e9d8d69971f981e4
周顺顺 <idioticzhou@foxmail.com>
2023-03-07 11:34:32 +0800

    feat(ui/button): optimize hover effect (#881)
    * feat(button): 为button按钮实现hover效果
    
    * fix(button): fix comment,The mask layer is implemented using a dynamic class,fix func name
    
    * test(snap): update snapshots

--- packages/varlet-ui/src/button/Button.vue
+++ packages/varlet-ui/src/button/Button.vue
@@ -18,6 +18,7 @@
         [states.outline, n('--outline')]
       )
     "
+    v-hover:desktop="handleHover"
     :style="{
       color: states.textColor,
       background: states.color,
@@ -39,6 +40,7 @@
     <div :class="classes(n('content'), [loading || pending, n('--hidden')])">
       <slot />
     </div>
+    <div :class="classes(n('overlay'), [hovering, n('--hovering')])"></div>
   </button>
 </template>
 
@@ -49,6 +51,7 @@ import { computed, defineComponent, ref, type Ref } from 'vue'
 import { props } from './props'
 import { call, createNamespace } from '../utils/components'
 import { useButtonGroup } from './provide'
+import Hover from '../hover'
 import { isArray } from '@varlet/shared'
 
 const { n, classes } = createNamespace('button')
@@ -58,10 +61,11 @@ export default defineComponent({
   components: {
     VarLoading,
   },
-  directives: { Ripple },
+  directives: { Ripple, Hover },
   props,
   setup(props) {
     const pending: Ref<boolean> = ref(false)
+    const hovering: Ref<boolean> = ref(false)
     const { buttonGroup } = useButtonGroup()
 
     const states = computed(() => {
@@ -126,11 +130,17 @@ export default defineComponent({
       attemptAutoLoading(call(onTouchstart, e))
     }
 
+    const handleHover = (isHover: boolean) => {
+      hovering.value = isHover
+    }
+
     return {
       n,
       classes,
       pending,
       states,
+      hovering,
+      handleHover,
       handleClick,
       handleTouchstart,
     }
