cc4554cace2c89c0a6e357efe9c987e0423f0fb9
haoziqaq <357229046@qq.com>
2023-03-15 21:23:21 +0800

    revert: fix workflow
    This reverts commit b6da6e88e2a885b9fc6e84cf9e438eaf0dfb40e1.

--- packages/varlet-ui-playground/vite.config.ts
+++ packages/varlet-ui-playground/vite.config.ts
@@ -1,7 +1,29 @@
+import fs from 'fs'
 import components from 'unplugin-vue-components/vite'
 import autoImport from 'unplugin-auto-import/vite'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+function toPath(path: string) {
+  return fileURLToPath(new URL(path, import.meta.url))
+}
+
+function copyVarletDependencies(): Plugin {
+  return {
+    name: 'copy-varlet-dependencies',
+
+    buildStart() {
+      fs.copyFileSync(toPath('../varlet-ui/es/varlet.esm.js'), toPath('./public/varlet.esm.js'))
+      fs.copyFileSync(toPath('../varlet-touch-emulator/index.js'), toPath('./public/varlet-touch-emulator.js'))
+      fs.copyFileSync(toPath('../varlet-ui/es/style.css'), toPath('./public/varlet.css'))
+      fs.writeFileSync(
+        toPath('./public/varlet-area.js'),
+        `export default ${fs.readFileSync(toPath('../varlet-ui/json/area.json'))}`
+      )
+    },
+  }
+}
 
 export default defineConfig({
   base: './',
@@ -16,6 +38,7 @@ export default defineConfig({
   },
 
   plugins: [
+    copyVarletDependencies(),
     components({
       resolvers: [VarletUIResolver()],
     }),
