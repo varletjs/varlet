b6da6e88e2a885b9fc6e84cf9e438eaf0dfb40e1
haoziqaq <357229046@qq.com>
2023-03-15 21:21:06 +0800

    chore: fix workflow

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,4 +1,5 @@
 import execa from 'execa'
+import fs from 'fs'
 import { createSpinner } from 'nanospinner'
 import { resolve } from 'path'
 
@@ -9,6 +10,8 @@ const PKG_ICONS = resolve(CWD, './packages/varlet-icons')
 const PKG_UI = resolve(CWD, './packages/varlet-ui')
 const PKG_SHARED = resolve(CWD, './packages/varlet-shared')
 const PKG_USE = resolve(CWD, './packages/varlet-use')
+const PKG_PLAYGROUND = resolve(CWD, './packages/varlet-ui-playground')
+const PKG_EMULATOR = resolve(CWD, './packages/varlet-touch-emulator')
 
 export const buildCli = () => execa('pnpm', ['build'], { cwd: PKG_CLI })
 
@@ -20,7 +23,20 @@ export const buildUse = () => execa('pnpm', ['build'], { cwd: PKG_USE })
 
 export const buildIcons = () => execa('pnpm', ['build'], { cwd: PKG_ICONS })
 
-export const buildUI = () => execa('pnpm', ['compile'], { cwd: PKG_UI })
+export const buildUI = async () => {
+  await execa('pnpm', ['compile'], { cwd: PKG_UI })
+  copyUIToPlayground()
+}
+
+export const copyUIToPlayground = () => {
+  fs.copyFileSync(resolve(PKG_UI, './es/varlet.esm.js'), resolve(PKG_PLAYGROUND, './public/varlet.esm.js'))
+  fs.copyFileSync(resolve(PKG_EMULATOR, './index.js'), resolve(PKG_PLAYGROUND, './public/varlet-touch-emulator.js'))
+  fs.copyFileSync(resolve(PKG_UI, './es/style.css'), resolve(PKG_PLAYGROUND, './public/varlet.css'))
+  fs.writeFileSync(
+    resolve(PKG_PLAYGROUND, './public/varlet-area.js'),
+    `export default ${fs.readFileSync(resolve(PKG_UI, './json/area.json'))}`
+  )
+}
 
 export async function runTask(taskName, task) {
   const s = createSpinner(`Building ${taskName}`).start()
