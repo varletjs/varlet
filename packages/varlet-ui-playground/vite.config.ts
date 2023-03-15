66c95df0c2147ea451e589ac93e6040a78411813
haoziqaq <357229046@qq.com>
2023-03-15 18:57:26 +0800

    chore: fix playground vite config

--- packages/varlet-ui-playground/vite.config.ts
+++ packages/varlet-ui-playground/vite.config.ts
@@ -1,24 +1,18 @@
 import fs from 'fs'
 import components from 'unplugin-vue-components/vite'
 import autoImport from 'unplugin-auto-import/vite'
-import { resolve } from 'path'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig, Plugin } from 'vite'
-
-const varletESMBundleFile = resolve(__dirname, '../varlet-ui/es/varlet.esm.js')
-const varletTouchEmulatorFile = resolve(__dirname, '../varlet-touch-emulator/index.js')
-const varletCSSFile = resolve(__dirname, '../varlet-ui/es/style.css')
-const varletArea = resolve(__dirname, '../varlet-ui/json/area.json')
 
 function copyVarletDependencies(): Plugin {
   return {
     name: 'copy-varlet-dependencies',
 
     buildStart() {
-      fs.copyFileSync(varletESMBundleFile, resolve('public/varlet.esm.js'))
-      fs.copyFileSync(varletTouchEmulatorFile, resolve('public/varlet-touch-emulator.js'))
-      fs.copyFileSync(varletCSSFile, resolve('public/varlet.css'))
-      fs.writeFileSync(resolve('public/varlet-area.js'), `export default ${fs.readFileSync(varletArea)}`)
+      fs.copyFileSync('../varlet-ui/es/varlet.esm.js', './public/varlet.esm.js')
+      fs.copyFileSync('../varlet-touch-emulator/index.js', './public/varlet-touch-emulator.js')
+      fs.copyFileSync('../varlet-ui/es/style.css', './public/varlet.css')
+      fs.writeFileSync('./public/varlet-area.js', `export default ${fs.readFileSync('../varlet-ui/json/area.json')}`)
     },
   }
 }
