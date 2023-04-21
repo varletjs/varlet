6e43190400ff65f094e234e40fba518258e9b4e7
EmberSpirit <wangkbweb@163.com>
2023-04-22 01:38:34 +0800

    refactor(use/useSticky): refactor useSticky

--- packages/varlet-use/src/useSticky.ts
+++ packages/varlet-use/src/useSticky.ts
@@ -1,46 +1,36 @@
 import { ref, Ref } from 'vue'
 
-type UseStickyHandleScroll = (
-  stickyOffsetTop: number,
-  stickyIsFixed: boolean,
-  remainOffsetTop: number,
-  parentScrollTop: number
-) => void
+type ScrollData = {
+  scrollTop: number
+  scrollRatio: number
+}
+type HandleScroll = (offsetTop: number, isFixed: boolean, scrollData: ScrollData) => void
 
 export type UseStickyDefault<T> = {
-  handleScroll: UseStickyHandleScroll
+  handleScroll: HandleScroll
   isFixed: Ref<boolean>
   options: T
 }
 
-type Getter<T> = (params: GetterParams) => T
+type Getter<T> = (params: GetterParams & ScrollData) => T
 
 type GetterParams = {
   stickyIsFixed: boolean
   stickyOffsetTop: number
-  remainOffsetTop: number
-  maxRemainOffsetTop: number
-  percentage: number
 }
 
 export const useSticky = function <T = any>(initialValue: T, getter: Getter<T>): UseStickyDefault<Ref<T | undefined>> {
   const isFixed = ref(false)
-  const offsetTop = ref(0)
-  const percentage = ref(1)
-  const maxRemainOffsetTop: Ref<number | undefined> = ref(undefined)
   const options: Ref<any> = ref(initialValue)
 
-  const handleScroll: UseStickyHandleScroll = (stickyOffsetTop, stickyIsFixed, remainOffsetTop, parentScrollTop) => {
-    maxRemainOffsetTop.value = remainOffsetTop + parentScrollTop
-    offsetTop.value = remainOffsetTop
+  const handleScroll: HandleScroll = (stickyOffsetTop, stickyIsFixed, scrollData) => {
+    console.log(stickyOffsetTop, stickyIsFixed, scrollData)
+
     isFixed.value = stickyIsFixed
-    percentage.value = remainOffsetTop / maxRemainOffsetTop.value
     options.value = getter({
       stickyIsFixed,
       stickyOffsetTop,
-      remainOffsetTop,
-      maxRemainOffsetTop: maxRemainOffsetTop.value,
-      percentage: percentage.value,
+      ...scrollData,
     })
   }
 
