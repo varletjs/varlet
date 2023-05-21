51f7a046d6f770fa0fd41997f82a0fd1fbe12544
gaoting <837082729@qq.com>
2023-05-21 10:17:31 +0800

    feat: github配置 + build配置解析

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,19 @@
+// 功能： 构建varlet项目中各个子模块代码
+
+// 执行命令行任务
 import execa from 'execa'
+// 创建运行任务时的 loading 动画。
 import { createSpinner } from 'nanospinner'
+// 获取指定路径的绝对路径。
 import { resolve } from 'path'
 
+// 定义了一个变量 CWD，其值为当前进程的工作目录
 const CWD = process.cwd()
+
+
+/**
+ * 定义了一个变量 PKG_xxx，其值为 varlet-xxx 模块的绝对路径
+ */
 const PKG_CLI = resolve(CWD, './packages/varlet-cli')
 const PKG_VITE_PLUGINS = resolve(CWD, './packages/varlet-vite-plugins')
 const PKG_ICONS = resolve(CWD, './packages/varlet-icons')
@@ -10,6 +21,9 @@ const PKG_UI = resolve(CWD, './packages/varlet-ui')
 const PKG_SHARED = resolve(CWD, './packages/varlet-shared')
 const PKG_USE = resolve(CWD, './packages/varlet-use')
 
+/**
+ * 定义了一个名为 buildxxx 的函数，其返回值是执行 pnpm build 命令在 varlet-xxx 模块下的输出结果
+ */
 export const buildCli = () => execa('pnpm', ['build'], { cwd: PKG_CLI })
 
 export const buildVitePlugins = () => execa('pnpm', ['build'], { cwd: PKG_VITE_PLUGINS })
@@ -23,6 +37,7 @@ export const buildIcons = () => execa('pnpm', ['build'], { cwd: PKG_ICONS })
 export const buildUI = () => execa('pnpm', ['compile'], { cwd: PKG_UI })
 
 export async function runTask(taskName, task) {
+  // createSpinner 函数：创建一个进度条，显示正在构建的的任务名称
   const s = createSpinner(`Building ${taskName}`).start()
   try {
     await task()
@@ -33,6 +48,9 @@ export async function runTask(taskName, task) {
   }
 }
 
+/**
+ * 按顺序执行以下所有任务。每个任务都会调用 runTask 函数进行构建
+ */
 export async function runTaskQueue() {
   await runTask('shared', buildShared)
   await runTask('use', buildUse)
