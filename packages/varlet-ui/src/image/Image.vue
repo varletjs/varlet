320ac6bc19bc8741af5fd8b24c23ea733f48c67d
binbin <binbin@MacBook-Pro.local>
2023-05-30 23:26:26 +0800

    refactor(ui/image): optimize code

--- packages/varlet-ui/src/image/Image.vue
+++ packages/varlet-ui/src/image/Image.vue
@@ -15,10 +15,9 @@
       :lazy-loading="loading"
       :lazy-error="error"
       :style="{ objectFit: fit }"
-      v-if="lazy && !isError"
+      v-if="lazy && !showErrorSlot"
       v-lazy="src"
       @load="handleLoad"
-      @error="handleError"
       @click="handleClick"
     />
 
@@ -28,14 +27,12 @@
       :title="title"
       :style="{ objectFit: fit }"
       :src="src"
-      v-if="!lazy && !isError"
+      v-if="!lazy && !showErrorSlot"
       @load="handleLoad"
       @error="handleError"
       @click="handleClick"
     />
-    <slot v-if="isError" name="error">
-      <img :class="n('image')" :alt="alt" :title="title" :style="{ objectFit: fit }" :src="src" />
-    </slot>
+    <slot v-if="showErrorSlot" name="error" />
   </div>
 </template>
 
@@ -57,17 +54,21 @@ export default defineComponent({
     Ripple,
   },
   props,
-  setup(props) {
-    const isError: Ref<boolean> = ref(false)
+  setup(props, { slots }) {
+    const showErrorSlot: Ref<boolean> = ref(false)
 
     const handleLoad = (e: Event) => {
       const el: LazyHTMLElement = e.currentTarget as LazyHTMLElement
-      const { lazy, error, onLoad, onError } = props
+      const { lazy, onLoad, onError } = props
 
       if (lazy) {
         el._lazy.state === 'success' && call(onLoad, e)
         if (el._lazy.state === 'error') {
-          isError.value = !error
+          if (slots?.error) {
+            // if you set error slot, the error image will not work
+            showErrorSlot.value = true
+          }
+
           call(onError, e)
         }
       } else {
@@ -76,12 +77,10 @@ export default defineComponent({
     }
 
     const handleError = (e: Event) => {
-      const { lazy, onError } = props
-
-      if (!lazy) {
-        isError.value = true
-        call(onError, e)
-      }
+      // this event is triggered by error event in default mode(not lazy)
+      // the value of showErrorSlot depends on whether there is an error slot
+      showErrorSlot.value = !!slots?.error
+      call(props.onError, e)
     }
 
     const handleClick = (e: Event) => {
@@ -91,14 +90,14 @@ export default defineComponent({
     watch(
       () => props.src,
       () => {
-        isError.value = false
+        showErrorSlot.value = false
       }
     )
 
     return {
       n,
       classes,
-      isError,
+      showErrorSlot,
       toSizeUnit,
       handleLoad,
       handleError,
