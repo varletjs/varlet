02947016752f4c685ec4f4aff577e5200ce5d883
dyggod <dyggod@163.com>
2023-06-01 11:07:26 +0800

    feat(extension): show document when hover (#1065)

--- packages/varlet-vscode-extension/src/hover.ts
+++ packages/varlet-vscode-extension/src/hover.ts
@@ -1,8 +1,16 @@
-import { bigCamelize, kebabCase } from '@varlet/shared'
-import { languages, Position, Hover, type TextDocument, type ExtensionContext } from 'vscode'
+import { kebabCase, bigCamelize } from '@varlet/shared'
+import { languages, Position, Hover, MarkdownString, type TextDocument, type ExtensionContext } from 'vscode'
 import { componentsMap } from './componentsMap'
-import { TAG_BIG_CAMELIZE_RE, DOCUMENTATION_EN, DOCUMENTATION_ZH, LANGUAGE_IDS, TAG_LINK_RE } from './constant'
+import {
+  TAG_BIG_CAMELIZE_RE,
+  DOCUMENTATION_EN,
+  DOCUMENTATION_ZH,
+  LANGUAGE_IDS,
+  TAG_LINK_RE,
+  LANGUAGES_TEXT,
+} from './constant'
 import { getLanguage } from './env'
+import { getWebTypesTags } from './completions'
 
 export function registerHover(context: ExtensionContext) {
   function provideHover(document: TextDocument, position: Position) {
@@ -10,21 +18,100 @@ export function registerHover(context: ExtensionContext) {
     const linkComponents = line.text.match(TAG_LINK_RE) ?? []
     const bigCamelizeComponents = line.text.match(TAG_BIG_CAMELIZE_RE) ?? []
     const components = [...new Set([...linkComponents, ...bigCamelizeComponents.map(kebabCase)])] as string[]
+    const isEnglish = getLanguage() === 'en-US'
+    const languageText = LANGUAGES_TEXT[getLanguage()]
+    const tags = getWebTypesTags()
 
     if (components.length) {
       const contents = components
         .filter((component) => componentsMap[component])
         .map((component) => {
+          const name = `var-${component}`
+          const tag = tags.find((tag) => tag.name === name)
           const { path } = componentsMap[component]
-          const language = getLanguage()
-          const name = bigCamelize(component)
+          const bigCamelName = bigCamelize(name)
+          const text = `${languageText.hover.linkText}:${bigCamelName}`
+          const documentation = isEnglish ? DOCUMENTATION_EN : DOCUMENTATION_ZH
+          const link = `[Varlet -> ${text}](${documentation}${path})`
 
-          const text = language === 'en-US' ? `Watch ${name} component documentation` : `查阅 ${name} 组件的官方文档`
-          const documentation = language === 'en-US' ? DOCUMENTATION_EN : DOCUMENTATION_ZH
+          if (!tag) {
+            return {
+              link,
+              props: [],
+              events: [],
+              slots: [],
+            }
+          }
 
-          return `[Varlet -> ${text}](${documentation}${path})`
+          const props = tag.attributes.map((attr) => {
+            return {
+              name: attr.name,
+              description: attr.description,
+              defaultValue: attr.default,
+            }
+          })
+
+          const events = tag.events.map((event) => {
+            return {
+              name: event.name,
+              description: event.description,
+            }
+          })
+
+          const slots = tag.slots
+            ? tag.slots.map((slot) => {
+                return {
+                  name: slot.name,
+                  description: slot.description,
+                }
+              })
+            : []
+
+          return {
+            link,
+            props,
+            events,
+            slots,
+          }
         })
-      return new Hover(contents)
+
+      const hoverContents: MarkdownString[] = []
+      contents
+        .map((item) => {
+          const { link, props, events, slots } = item
+          const { hover: hoverText } = languageText
+          let propsTableStr = `| ${hoverText.propname} | ${hoverText.description} | ${hoverText.default} | \n`
+          let eventsTableStr = `| ${hoverText.eventname} | ${hoverText.description} | \n`
+          let slotsTableStr = `| ${hoverText.slotname} | ${hoverText.description} | \n`
+          propsTableStr += `| :--- | :--- | :--- | \n`
+          eventsTableStr += `| :--- | :--- | \n`
+          slotsTableStr += `| :--- | :--- | \n`
+          props.forEach((prop) => {
+            propsTableStr += `| ${prop.name} | ${prop.description} | ${prop.defaultValue} | \n`
+          })
+          events.forEach((event) => {
+            eventsTableStr += `| ${event.name} | ${event.description} | \n`
+          })
+          slots.forEach((slot) => {
+            slotsTableStr += `| ${slot.name} | ${slot.description} | \n`
+          })
+          return {
+            link,
+            propsTableStr,
+            eventsTableStr,
+            slotsTableStr,
+          }
+        })
+        .forEach((item) => {
+          hoverContents.push(
+            new MarkdownString(item.link),
+            new MarkdownString(item.propsTableStr),
+            new MarkdownString(item.eventsTableStr),
+            new MarkdownString(item.slotsTableStr)
+          )
+        })
+
+      return new Hover(hoverContents)
     }
   }
 
