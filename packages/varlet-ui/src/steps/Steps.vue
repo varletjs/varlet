f93d524d6e141485a8742e4456b175eaeef61ea3
chenkeyu <1140051355@qq.com>
2023-06-07 12:31:00 +0800

    fix: fix step line style bug in Step/Step

--- packages/varlet-ui/src/steps/Steps.vue
+++ packages/varlet-ui/src/steps/Steps.vue
@@ -1,16 +1,18 @@
 <template>
-  <div :class="n()" :style="{ flexDirection: direction === 'horizontal' ? 'row' : 'column' }">
+  <div ref="stepsEl" :class="n()" :style="{ flexDirection: direction === 'horizontal' ? 'row' : 'column' }">
     <slot />
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref, watch, onActivated } from 'vue'
 import { useStep } from './provide'
 import { props } from './props'
-import type { ComputedRef } from 'vue'
+import type { ComputedRef, Ref } from 'vue'
 import type { StepsProvider } from './provide'
 import { createNamespace, call } from '../utils/components'
+import { doubleRaf } from '../utils/elements'
+import { useEventListener } from '@varlet/use'
 
 const { n } = createNamespace('steps')
 
@@ -22,9 +24,9 @@ export default defineComponent({
     const activeColor: ComputedRef<string | undefined> = computed(() => props.activeColor)
     const inactiveColor: ComputedRef<string | undefined> = computed(() => props.inactiveColor)
     const direction: ComputedRef<string> = computed(() => props.direction)
-
+    const size: Ref<number> = ref(0)
     const { length, bindStep } = useStep()
-
+    const stepsEl: Ref<HTMLElement | null> = ref(null)
     const clickStep = (index: number) => {
       call(props.onClickStep, index)
     }
@@ -36,11 +38,34 @@ export default defineComponent({
       activeColor,
       inactiveColor,
       clickStep,
+      size,
     }
 
     bindStep(stepsProvider)
+    const resize = () => {
+      if (!stepsEl.value) {
+        return
+      }
+      if (direction.value === 'horizontal') {
+        const itemStepSize = (stepsEl.value as HTMLDivElement).offsetWidth / length.value
+        size.value = parseInt(itemStepSize.toString(), 10)
+      }
+    }
+    watch(
+      () => length.value,
+      async () => {
+        // In nuxt, the size of the swipe cannot got when the route is change, need double raf
+        await doubleRaf()
+
+        resize()
+      }
+    )
+    onActivated(resize)
+    useEventListener(() => window, 'resize', resize)
     return {
       n,
+      resize,
+      stepsEl,
     }
   },
 })
