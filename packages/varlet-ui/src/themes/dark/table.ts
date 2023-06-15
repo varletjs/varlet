06d07a0529512fba248e26e96a0b88f515e1580e
lxing4812 <lxing4812@outlook.com>
2023-06-15 22:57:39 +0800

    fix(ui/table): fix css color inherit (#1098)

--- packages/varlet-ui/src/themes/dark/table.ts
+++ packages/varlet-ui/src/themes/dark/table.ts
@@ -1,6 +1,7 @@
 export default {
   '--table-background': '#303030',
   '--table-thead-th-text-color': 'rgba(255, 255, 255, 0.6)',
+  '--table-tbody-td-text-color': '#fff',
   '--table-tbody-tr-hover-background': '#4c4b4b',
   '--table-thead-border-bottom': 'thin solid rgba(255, 255, 255, 0.12)',
   '--table-tbody-tr-border-bottom': 'thin solid rgba(255, 255, 255, 0.12)',
