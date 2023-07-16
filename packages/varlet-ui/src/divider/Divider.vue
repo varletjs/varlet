3b8703ce94c932ea492839464e400bde15e94533
binbin <binbin@MacBook-Pro.local>
2023-07-16 10:44:32 +0800

    refactor(divider): optimize code

--- packages/varlet-ui/src/divider/Divider.vue
+++ packages/varlet-ui/src/divider/Divider.vue
@@ -6,7 +6,7 @@
         n('$--box'),
         [vertical, n('--vertical')],
         [withText, n('--with-text')],
-        [validInset, n('--inset')],
+        [withPresetInset, n('--inset')],
         [dashed, n('--dashed')],
         [hairline, n('--hairline')]
       )
@@ -14,7 +14,7 @@
     :style="style"
   >
     <slot v-if="!vertical">
-      <span :class="n('text')" v-if="description && !vertical">{{ description }}</span>
+      <span :class="n('text')" v-if="description">{{ description }}</span>
     </slot>
   </div>
 </template>
@@ -22,7 +22,7 @@
 <script lang="ts">
 import { defineComponent, computed, ref, onUpdated, type Ref } from 'vue'
 import { toSizeUnit } from '../utils/elements'
-import { toNumber } from '@varlet/shared'
+import { toNumber, isBoolean } from '@varlet/shared'
 import { props } from './props'
 import { createNamespace } from '../utils/components'
 import { onSmartMounted } from '@varlet/use'
@@ -35,7 +35,7 @@ export default defineComponent({
   setup(props, { slots }) {
     const withText: Ref<boolean> = ref(false)
 
-    const validInset = computed(() => {
+    const withPresetInset = computed(() => {
       // the inset is only effective in horizontal mode
       const { vertical, inset } = props
       return !vertical && inset === true
@@ -44,7 +44,7 @@ export default defineComponent({
     const style = computed(() => {
       const { inset, vertical, margin } = props
 
-      if (inset === true || vertical) {
+      if (isBoolean(inset) || vertical) {
         return { margin }
       }
 
@@ -77,7 +77,7 @@ export default defineComponent({
       classes,
       withText,
       style,
-      validInset,
+      withPresetInset,
     }
   },
 })
