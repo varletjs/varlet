b4d3061377f680568f69d8d005e51e1896044743
haoziqaq <357229046@qq.com>
2023-07-18 17:44:25 +0800

    style: remove unuseful code

--- packages/varlet-ui/src/swipe-item/index.ts
+++ packages/varlet-ui/src/swipe-item/index.ts
@@ -1,10 +1,6 @@
 import SwipeItem from './SwipeItem.vue'
 import { withInstall } from '../utils/components'
 
-SwipeItem.install = function (app: App) {
-  app.component(SwipeItem.name, SwipeItem)
-}
-
 withInstall(SwipeItem)
 
 export const _SwipeItemComponent = SwipeItem
