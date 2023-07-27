c4e832a03d836079e11339f0c9620ed84df21158
haoziqaq <357229046@qq.com>
2023-07-28 03:40:12 +0800

    refactor(popover): disable teleport on keepalive activated

--- packages/varlet-ui/src/tooltip/Tooltip.vue
+++ packages/varlet-ui/src/tooltip/Tooltip.vue
@@ -8,7 +8,7 @@
   >
     <slot />
 
-    <Teleport :to="teleport">
+    <Teleport :to="teleport" :disabled="teleportDisabled">
       <transition :name="n()" @after-enter="onOpened" @after-leave="handleClosed">
         <div
           ref="popover"
@@ -32,7 +32,7 @@
 </template>
 
 <script lang="ts">
-import { createNamespace } from '../utils/components'
+import { createNamespace, useTeleport } from '../utils/components'
 import { defineComponent } from 'vue'
 import { toSizeUnit } from '../utils/elements'
 import { usePopover } from '../menu/usePopover'
@@ -44,6 +44,7 @@ export default defineComponent({
   name: 'VarTooltip',
   props,
   setup(props) {
+    const { disabled: teleportDisabled } = useTeleport()
     const {
       popover,
       host,
@@ -72,6 +73,7 @@ export default defineComponent({
       hostSize,
       show,
       zIndex,
+      teleportDisabled,
       n,
       classes,
       handleHostClick,
