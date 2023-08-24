e6ab85e5657b6e53a4928831a4fb542b29722874
haoziqaq <357229046@qq.com>
2023-08-24 22:45:03 +0800

    style: clean code

--- packages/varlet-ui/src/switch/Switch.vue
+++ packages/varlet-ui/src/switch/Switch.vue
@@ -53,8 +53,8 @@
 <script lang="ts">
 import VarFormDetails from '../form-details'
 import Ripple from '../ripple'
-import VarHoverOverlay, { useHoverOverlay } from '../hover-overlay'
 import Hover from '../hover'
+import VarHoverOverlay, { useHoverOverlay } from '../hover-overlay'
 import { defineComponent, computed, nextTick, type ComputedRef } from 'vue'
 import { useValidation, createNamespace, call } from '../utils/components'
 import { multiplySizeUnit } from '../utils/elements'
@@ -136,15 +136,22 @@ export default defineComponent({
       } = props
 
       call(onClick, event)
-      if (disabled || loading || readonly || form?.disabled.value || form?.readonly.value) return
+
+      if (disabled || loading || readonly || form?.disabled.value || form?.readonly.value) {
+        return
+      }
+
       const newValue = modelValue === activeValue ? inactiveValue : activeValue
+
       call(onChange, newValue)
       call(updateModelValue, newValue)
       validateWithTrigger()
     }
 
     const hover = (value: boolean) => {
-      if (props.disabled || form?.disabled.value) return
+      if (props.disabled || form?.disabled.value) {
+        return
+      }
 
       handleHovering(value)
     }
@@ -159,20 +166,21 @@ export default defineComponent({
       validate,
       resetValidation,
     }
+
     call(bindForm, switchProvider)
 
     return {
       n,
       classes,
-      multiplySizeUnit,
-      switchActive,
       hovering,
-      hover,
       radius,
       styleComputed,
       errorMessage,
       formDisabled: form?.disabled,
       formReadonly: form?.readonly,
+      multiplySizeUnit,
+      switchActive,
+      hover,
     }
   },
 })
