2f7ee3924b9308816f18307bbe0e4d7f00088fdc
haoziqaq <357229046@qq.com>
2023-09-19 23:41:38 +0800

    refactor(use): clean code

--- packages/varlet-use/src/useId.ts
+++ packages/varlet-use/src/useId.ts
@@ -4,11 +4,9 @@ import { kebabCase } from '@varlet/shared'
 export function useId() {
   const id = ref<string>()
   const instance = getCurrentInstance()!
-
-  // @ts-ignore
-  const isTestEnv = process.env.NODE_ENV === 'test'
   const name = kebabCase(instance.type.name!)
-  id.value = isTestEnv ? name : `${name}-${instance.uid}`
+
+  id.value = process.env.NODE_ENV === 'test' ? name : `${name}-${instance.uid}`
 
   return id
 }
