2d977327fb51428eda67091d0c4996af6bde28c1
Jimmy <jimmyrss1102@gmail.com>
2023-10-17 23:05:21 +0800

    chore: remove excess parameters

--- packages/varlet-ui/src/avatar-group/AvatarGroup.vue
+++ packages/varlet-ui/src/avatar-group/AvatarGroup.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    :class="classes(n(), [hoverable, n('--hover-able'), null], [vertical, n('--column'), n('--row')])"
-    :style="rootStyles"
-  >
+  <div :class="classes(n(), [hoverable, n('--hover-able')], [vertical, n('--column'), n('--row')])" :style="rootStyles">
     <slot />
   </div>
 </template>
