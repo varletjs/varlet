635cf536a06c8a5eeb3f4763bd9b38a4202072e0
gaoting <837082729@qq.com>
2023-10-20 14:35:29 +0800

    feat: reset

--- packages/varlet-ui/src/picker-group/__tests__/index.spec.js
+++
@@ -1,8 +0,0 @@
-import PickerGroup from '..'
-import { createApp } from 'vue'
-import { mount } from '@vue/test-utils'
-
-test('test picker-group plugin', () => {
-  const app = createApp({}).use(PickerGroup)
-  expect(app.component(PickerGroup.name)).toBeTruthy()
-})
