1778a1ce746ef88b68c98d8a3527bf657662fc0c
chouchouji <1305974212@qq.com>
2023-11-15 20:54:40 +0800

    docs(rate): typo

--- packages/varlet-ui/src/rate/example/index.vue
+++ packages/varlet-ui/src/rate/example/index.vue
@@ -32,20 +32,20 @@ watchDarkMode(Themes.dark)
 </script>
 
 <template>
-  <app-type>{{ pack.baseRating }}</app-type>
+  <app-type>{{ pack.basicUsage }}</app-type>
   <var-rate v-model="scores.score" />
   <var-rate v-model="scores.score" icon="thumb-up" empty-icon="thumb-up-outline" />
 
-  <app-type>{{ pack.customizeTheTotalNumberOfRatingICONS }}</app-type>
+  <app-type>{{ pack.customTotalRating }}</app-type>
   <var-rate v-model="scores.score1" :count="8" />
 
-  <app-type>{{ pack.customizeRatingIconStyles }}</app-type>
+  <app-type>{{ pack.customRatingIconStyle }}</app-type>
   <var-rate v-model="scores.score3" icon="heart" empty-icon="heart-outline" color="red" empty-color="red" />
 
-  <app-type>{{ pack.customizeRatingIconSize }}</app-type>
+  <app-type>{{ pack.customRatingIconSizeAndGap }}</app-type>
   <var-rate v-model="scores.score4" size="8vmin" gap="1vmin" />
 
-  <app-type>{{ pack.useHalfAStar }}</app-type>
+  <app-type>{{ pack.halfStar }}</app-type>
   <var-rate v-model="scores.score6" :count="8" half />
   <var-rate
     v-model="scores.score6"
@@ -58,16 +58,16 @@ watchDarkMode(Themes.dark)
     style="margin-top: 4px"
   />
 
-  <app-type>{{ pack.disableTheRating }}</app-type>
+  <app-type>{{ pack.disabledAndReadonly }}</app-type>
   <var-rate v-model="scores.score7" disabled />
   <var-rate v-model="scores.score8" readonly />
 
   <app-type>{{ pack.clearable }}</app-type>
   <var-rate v-model="scores.score12" clearable half />
 
-  <app-type>{{ pack.listeningEvents }}</app-type>
+  <app-type>{{ pack.listenEvent }}</app-type>
   <var-rate v-model="scores.score10" @change="handleChange" />
 
   <app-type>{{ pack.validate }}</app-type>
-  <var-rate :rules="[(v) => v >= 3 || pack.rateMessage]" v-model="scores.score11" />
+  <var-rate :rules="[(v) => v >= 3 || pack.errorMessage]" v-model="scores.score11" />
 </template>
