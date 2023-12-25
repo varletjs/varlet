c3f2ba6de02916b2e81c1169bad97c0bb7d23fa0
haoziqaq <357229046@qq.com>
2023-12-26 00:25:29 +0800

    docs(image): support md3

--- packages/varlet-ui/src/image/example/index.vue
+++ packages/varlet-ui/src/image/example/index.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { Themes } from '@varlet/ui'
-import { AppType, watchLang, watchDarkMode } from '@varlet/cli/client'
+import { AppType, watchLang, onThemeChange } from '@varlet/cli/client'
 import { use, pack } from './locale'
 
 watchLang(use)
-watchDarkMode(Themes.dark)
+onThemeChange()
 </script>
 
 <template>
