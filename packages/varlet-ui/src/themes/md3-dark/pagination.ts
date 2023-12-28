e29bcf745a834f5b5e80031e8a19765e177db6fa
haoziqaq <357229046@qq.com>
2023-12-29 03:01:57 +0800

    feat(pagination): fix cssvar

--- packages/varlet-ui/src/themes/md3-dark/pagination.ts
+++ packages/varlet-ui/src/themes/md3-dark/pagination.ts
@@ -2,7 +2,7 @@ export default {
   '--pagination-text-color': '#fff',
   '--pagination-active-color': '#381E72',
   '--pagination-list-bg-color': '#211F26',
-  '--pagination-list-active-bg-color': '#4F378B',
-  '--pagination-list-active-color': '#E8DEF8',
+  '--pagination-list-active-bg-color': 'rgba(208, 188, 255, .2)',
+  '--pagination-list-active-color': '#D0BCFF',
   '--pagination-item-background': '#211F26',
 }
