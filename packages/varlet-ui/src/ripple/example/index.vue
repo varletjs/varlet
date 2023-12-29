a2c93e46f1b584ba186fdbb88185a4b7b8fcd757
haoziqaq <357229046@qq.com>
2023-12-29 18:34:41 +0800

    docs(ripple): support md3

--- packages/varlet-ui/src/ripple/example/index.vue
+++ packages/varlet-ui/src/ripple/example/index.vue
@@ -1,9 +1,10 @@
 <script setup>
 import vRipple from '..'
-import { AppType, watchLang } from '@varlet/cli/client'
+import { AppType, watchLang, onThemeChange } from '@varlet/cli/client'
 import { pack, use } from './locale'
 
 watchLang(use)
+onThemeChange()
 </script>
 
 <template>
@@ -11,7 +12,9 @@ watchLang(use)
   <div class="ripple-example__block var-elevation--2" v-ripple>{{ pack.click }}</div>
 
   <app-type>{{ pack.customColor }}</app-type>
-  <div class="ripple-example__block var-elevation--2" v-ripple="{ color: '#2979ff' }">{{ pack.click }}</div>
+  <div class="ripple-example__block var-elevation--2" v-ripple="{ color: 'var(--color-warning)' }">
+    {{ pack.click }}
+  </div>
 
   <app-type>{{ pack.disabledStatus }}</app-type>
   <div class="ripple-example__block var-elevation--2" v-ripple="{ disabled: true }">{{ pack.click }}</div>
@@ -25,11 +28,10 @@ watchLang(use)
   justify-content: center;
   align-items: center;
   height: 100px;
-  color: var(--site-config-color-sub-text);
-  background: var(--site-config-color-sub-bar);
   user-select: none;
   cursor: pointer;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  background-color: var(--card-background);
   transition: background-color 0.25s;
 }
 </style>
