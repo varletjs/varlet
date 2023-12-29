6b774e28a8029f553cedf47e2c6e244a4a23bb75
haoziqaq <357229046@qq.com>
2023-12-29 17:45:20 +0800

    docs(index-bar): support md3

--- packages/varlet-ui/src/index-bar/example/index.vue
+++ packages/varlet-ui/src/index-bar/example/index.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { Themes } from '@varlet/ui'
 import { ref, onMounted } from 'vue'
 import { pack, use } from './locale'
-import { AppType, watchLang, watchDarkMode } from '@varlet/cli/client'
+import { AppType, watchLang, onThemeChange } from '@varlet/cli/client'
 
 const list = ref([])
-const bgColor = ref('#e7edf7')
-const color = ref('#2e67ba')
 
 onMounted(() => {
   for (let i = 0; i < 26; i++) {
@@ -14,22 +11,23 @@ onMounted(() => {
   }
 })
 
+watchLang(use)
+onThemeChange()
+
 function handleChange(value) {
   console.log(value)
 }
-
-watchLang(use)
-watchDarkMode(Themes.dark, (theme) => {
-  bgColor.value = theme === 'darkTheme' ? 'rgb(41, 42, 45)' : '#e7edf7'
-  color.value = theme === 'darkTheme' ? '#3980e8' : '#2e67ba'
-})
 </script>
 
 <template>
   <app-type>{{ pack.basicUsage }}</app-type>
   <var-index-bar @change="handleChange" duration="300" :sticky-offset-top="54">
     <div v-for="item in list" :key="item">
-      <var-index-anchor :index="item" class="index-bar-example-anchor" :style="{ background: bgColor, color }">
+      <var-index-anchor
+        :index="item"
+        class="index-bar-example-anchor"
+        :style="{ background: 'var(--chip-primary-color)', color: 'var(--chip-primary-text-color)' }"
+      >
         {{ pack.title }} {{ item }}
       </var-index-anchor>
       <var-cell>{{ item }} {{ pack.text }}</var-cell>
