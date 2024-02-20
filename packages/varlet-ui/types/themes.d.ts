ff2f7d12cbde8eff005966fecf764ef08b16af6e
haoziqaq <357229046@qq.com>
2024-02-20 11:13:49 +0800

    feat(themes): support toViewport

--- packages/varlet-ui/types/themes.d.ts
+++ packages/varlet-ui/types/themes.d.ts
@@ -1,9 +1,17 @@
 import { StyleVars } from './styleProvider'
 
+export interface ThemesToViewportOptions {
+  viewportWidth?: number
+  viewportUnit?: string
+  unitPrecision?: number
+}
+
 interface Themes {
   dark: StyleVars
   md3Light: StyleVars
   md3Dark: StyleVars
+
+  toViewport(theme: StyleVars, options?: ThemesToViewportOptions): StyleVars
 }
 
 export declare const Themes: Themes
