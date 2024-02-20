c02e02d25589c238352ddcdb72cf7f448819bca9
haoziqaq <357229046@qq.com>
2024-02-20 12:24:56 +0800

    chore: close playground

--- packages/varlet-ui/varlet.config.mjs
+++ packages/varlet-ui/varlet.config.mjs
@@ -7,10 +7,7 @@ export default defineConfig({
   },
   pc: {
     header: {
-      playground:
-        process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3000'
-          : 'https://varlet.gitee.io/varlet-ui-playground',
+      playground: null,
     },
     menu: [
       {
