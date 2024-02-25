16d906b89c74f1e13f5b225bfdcc47ea11878a81
haoziqaq <357229046@qq.com>
2024-02-26 05:36:40 +0800

    fix(overlay): fix overlazy z-index

--- packages/varlet-ui/src/overlay/Overlay.tsx
+++ packages/varlet-ui/src/overlay/Overlay.tsx
@@ -33,7 +33,7 @@ export default defineComponent({
         <div
           class={n()}
           style={{
-            zIndex: zIndex.value - 1,
+            zIndex: zIndex.value,
           }}
           {...attrs}
           onClick={handleClickOverlay}
