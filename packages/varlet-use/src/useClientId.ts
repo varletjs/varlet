e44b40c52b1aa0b4a55c1cc425d385d64d949504
haoziqaq <357229046@qq.com>
2024-04-25 02:42:18 +0800

    fix(use): fix non unique id warning in browser

--- packages/varlet-use/src/useClientId.ts
+++ packages/varlet-use/src/useClientId.ts
@@ -4,7 +4,7 @@ import { kebabCase } from '@varlet/shared'
 export function useClientId() {
   const instance = getCurrentInstance()!
   const name = kebabCase(instance.type.name!)
-  const id = ref<string>(process.env.NODE_ENV === 'test' ? `${name}-mock-id` : '')
+  const id = ref<string | undefined>(process.env.NODE_ENV === 'test' ? `${name}-mock-id` : undefined)
 
   onMounted(() => {
     if (process.env.NODE_ENV !== 'test') {
