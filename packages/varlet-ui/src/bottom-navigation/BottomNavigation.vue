8a3538787f48e38e5ed8d5dc22b15d62b24ffd75
Jackie Qian <717618740@qq.com>
2024-05-14 11:28:53 +0800

    feat(bottom-navigation): optimize code

--- packages/varlet-ui/src/bottom-navigation/BottomNavigation.vue
+++ packages/varlet-ui/src/bottom-navigation/BottomNavigation.vue
@@ -28,7 +28,7 @@
     </var-button>
   </div>
 
-  <div v-if="fixed && placeholder" :class="classes(n('--placeholder'))" :style="{ height: placeholderHeight }" />
+  <div v-if="fixed && placeholder" class="var-bottom-navigation--placeholder" :style="{ height: placeholderHeight }" />
 </template>
 
 <script lang="ts">
