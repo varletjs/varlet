6f7477758fe9bc6f25c80866a53d7bda03b8b739
haoziqaq <357229046@qq.com>
2024-05-30 16:40:41 +0800

    refactor: optimize mobile css

--- packages/varlet-cli/site/pc/components/AppMobile.vue
+++ packages/varlet-cli/site/pc/components/AppMobile.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="varlet-site-mobile varlet-site-mobile--375">
     <div class="varlet-site-mobile-content">
-      <iframe id="mobile" :src="`${getMobileIndex()}#/${componentName}?language=${language}&platform=pc&replace=${replace}${hash ? `#${hash}` : ''}`"></iframe>
+      <iframe
+        id="mobile"
+        :src="`${getMobileIndex()}#/${componentName}?language=${language}&platform=pc&replace=${replace}${
+          hash ? `#${hash}` : ''
+        }`"
+      ></iframe>
     </div>
   </div>
 </template>
@@ -13,23 +18,23 @@ export default {
   name: 'AppMobile',
   props: {
     componentName: {
-      type: String
+      type: String,
     },
     language: {
-      type: String
+      type: String,
     },
     replace: {
-      type: String
+      type: String,
     },
     hash: {
-      type: String
-    }
+      type: String,
+    },
   },
   setup() {
     return {
-      getMobileIndex
+      getMobileIndex,
     }
-  }
+  },
 }
 </script>
 
@@ -43,6 +48,7 @@ export default {
   overflow: hidden;
   box-shadow: 0 0 14px 6px var(--site-config-color-shadow);
   border-radius: 2px;
+  margin-top: 20px;
 
   @media screen and (min-width: 1600px) {
     &--375 {
