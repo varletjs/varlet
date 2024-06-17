ae36ea4dedee698f7680b9db2b670fc3e8b537e0
haoziqaq <357229046@qq.com>
2024-06-18 01:58:32 +0800

    fix: fix md3 light theme format error

--- packages/varlet-ui/src/themes/md3-light/picker.ts
+++ packages/varlet-ui/src/themes/md3-light/picker.ts
@@ -2,7 +2,7 @@ export default {
   '--picker-background': 'var(--color-body)',
   '--picker-cancel-button-text-color': 'var(--color-primary)',
   '--picker-mask-background-image':
-    'linear-gradient(180deg, rgba(254, 247, 255, 0.9), rgba(254, 247, 255, 0.4)), linear-gradient(0deg, rgba(254, 247, 255, 0.9), rgba(254, 247, 255, 0.4)',
+    'linear-gradient(180deg, rgba(254, 247, 255, 0.9), rgba(254, 247, 255, 0.4)), linear-gradient(0deg, rgba(254, 247, 255, 0.9), rgba(254, 247, 255, 0.4))',
   '--picker-toolbar-height': '44px',
   '--picker-confirm-button-text-color': 'var(--color-primary)',
   '--picker-picked-border': '1px solid var(--color-outline)',
