a0d5888849d91c42363ac5a4045671a6b9e58755
chouchouji <1305974212@qq.com>
2024-07-12 11:33:45 +0800

    docs(themes): optimize docs

--- packages/varlet-ui/src/themes/docs/zh-CN.md
+++ packages/varlet-ui/src/themes/docs/zh-CN.md
@@ -23,10 +23,10 @@ import { StyleProvider, Themes } from '@varlet/ui'
 
 <template>
   <var-space direction="column" size="large">
-    <var-button block @click="() => StyleProvider(null)">Material Design 2 亮色</var-button>
-    <var-button block @click="() => StyleProvider(Themes.dark)">Material Design 2 暗色</var-button>
-    <var-button block @click="() => StyleProvider(Themes.md3Light)">Material Design 3 亮色</var-button>
-    <var-button block @click="() => StyleProvider(Themes.md3Dark)">Material Design 3 暗色</var-button>
+    <var-button type="primary" block @click="StyleProvider(null)">Material Design 2 亮色</var-button>
+    <var-button type="primary" block @click="StyleProvider(Themes.dark)">Material Design 2 暗色</var-button>
+    <var-button type="primary" block @click="StyleProvider(Themes.md3Light)">Material Design 3 亮色</var-button>
+    <var-button type="primary" block @click="StyleProvider(Themes.md3Dark)">Material Design 3 暗色</var-button>
   </var-space>
 </template>
 ```
--- packages/varlet-ui/src/themes/example/index.vue
+++ packages/varlet-ui/src/themes/example/index.vue
@@ -13,15 +13,9 @@ function setTheme(theme) {
 <template>
   <app-type>{{ t('toggleTheme') }}</app-type>
   <var-space direction="column" size="large">
-    <var-button type="primary" block @click="() => setTheme('lightTheme')"
-      >Material Design 2 {{ t('light') }}</var-button
-    >
-    <var-button type="primary" block @click="() => setTheme('darkTheme')">Material Design 2 {{ t('dark') }}</var-button>
-    <var-button type="primary" block @click="() => setTheme('md3LightTheme')"
-      >Material Design 3 {{ t('light') }}</var-button
-    >
-    <var-button type="primary" block @click="() => setTheme('md3DarkTheme')"
-      >Material Design 3 {{ t('dark') }}</var-button
-    >
+    <var-button type="primary" block @click="setTheme('lightTheme')">Material Design 2 {{ t('light') }}</var-button>
+    <var-button type="primary" block @click="setTheme('darkTheme')">Material Design 2 {{ t('dark') }}</var-button>
+    <var-button type="primary" block @click="setTheme('md3LightTheme')">Material Design 3 {{ t('light') }}</var-button>
+    <var-button type="primary" block @click="setTheme('md3DarkTheme')">Material Design 3 {{ t('dark') }}</var-button>
   </var-space>
 </template>
