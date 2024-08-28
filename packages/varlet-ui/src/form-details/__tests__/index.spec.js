da7bddee26bbecb9aa0afbb9b60df970641b2f19
Daizi <105732536+Daizi0905@users.noreply.github.com>
2024-08-28 19:10:28 +0800

    test(form-details): complete test cases (#1740)
    * test(form-details): complete test cases
    
    * fix(form-details): add missing imports
    
    * fix(form-details): rename slot name

--- packages/varlet-ui/src/form-details/__tests__/index.spec.js
+++ packages/varlet-ui/src/form-details/__tests__/index.spec.js
@@ -1,8 +1,68 @@
 import FormDetails from '..'
+import { mount } from '@vue/test-utils'
 import { createApp } from 'vue'
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
 test('test form details plugin', () => {
   const app = createApp({}).use(FormDetails)
   expect(app.component(FormDetails.name)).toBeTruthy()
+})
+
+describe('test form details component props', () => {
+  test('test form details errorMessage', async () => {
+    const wrapper = mount(FormDetails, {
+      props: {
+        errorMessage: 'Initial errorMessage',
+      },
+    })
+
+    const errorMessageWrapper = wrapper.find('.var-form-details__error-message')
+    const errorMessageDiv = errorMessageWrapper.find('div')
+
+    expect(errorMessageDiv.text()).toBe('Initial errorMessage')
+
+    await wrapper.setProps({
+      errorMessage: 'Updated errorMessage',
+    })
+
+    expect(errorMessageDiv.text()).toBe('Updated errorMessage')
+
+    wrapper.unmount()
+  })
+
+  test('test form details extraMessage', async () => {
+    const wrapper = mount(FormDetails, {
+      props: {
+        extraMessage: 'Initial extraMessage',
+      },
+    })
+
+    const extraMessageWrapper = wrapper.find('.var-form-details__extra-message')
+    const extraMessageDiv = extraMessageWrapper.find('div')
+
+    expect(extraMessageDiv.text()).toBe('Initial extraMessage')
+
+    await wrapper.setProps({
+      extraMessage: 'Updated extraMessage',
+    })
+
+    expect(extraMessageDiv.text()).toBe('Updated extraMessage')
+
+    wrapper.unmount()
+  })
+})
+
+describe('test form details component slots', () => {
+  test('test chip extraMessage slots', () => {
+    const wrapper = mount(FormDetails, {
+      slots: {
+        'extra-message': () => 'This is extra message slot',
+      },
+    })
+
+    const extraMessageWrapper = wrapper.find('.var-form-details__extra-message')
+    expect(extraMessageWrapper.text()).toBe('This is extra message slot')
+
+    wrapper.unmount()
+  })
 })
