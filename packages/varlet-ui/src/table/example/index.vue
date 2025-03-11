89bfefc057aa27028189df52756fbd9ac4714b36
haoziqaq <357229046@qq.com>
2025-03-11 22:10:40 +0800

    docs: fix docs

--- packages/varlet-ui/src/table/example/index.vue
+++ packages/varlet-ui/src/table/example/index.vue
@@ -4,6 +4,7 @@ import { AppType, onThemeChange, watchLang } from '@varlet/cli/client'
 import { t, use } from './locale'
 
 const list = ref(gen(1, 10))
+const list2 = ref(gen(1, 10))
 
 function gen(current, size) {
   return Array.from({ length: size }).map((_, index) => {
@@ -19,6 +20,10 @@ function gen(current, size) {
 
 function get(current, size) {
   list.value = gen(current, size)
+}
+
+function get2(current, size) {
+  list2.value = gen(current, size)
 }
 
 watchLang(use)
@@ -84,7 +89,7 @@ onThemeChange()
       </tr>
     </thead>
     <tbody>
-      <tr v-for="l in list" :key="l.name">
+      <tr v-for="l in list2" :key="l.name">
         <td>{{ l.name }}</td>
         <td>{{ l.math }}</td>
         <td>{{ l.english }}</td>
@@ -93,7 +98,7 @@ onThemeChange()
 
     <template #footer>
       <div class="footer">
-        <var-pagination :current="1" :total="100" :size-option="[5, 10]" @change="get" />
+        <var-pagination :current="1" :total="100" :size-option="[5, 10]" @change="get2" />
       </div>
     </template>
   </var-table>
