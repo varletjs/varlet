6ab2c9df025e9ca21dbd6bfff1be0d249f143803
xueyuan <xueyuan@vanchen.com>
2025-07-10 16:18:57 +0800

    feat(input): 为轮廓输入框添加全选功能
    添加outlinedInputRef引用和selectAllOutlined方法，使轮廓样式的输入框也能支持全选功能

--- packages/varlet-ui/src/input/example/index.vue
+++ packages/varlet-ui/src/input/example/index.vue
@@ -34,11 +34,17 @@ const outlinedValue11 = ref('')
 const outlinedValue12 = ref('')
 const outlinedValue13 = ref('')
 const outlinedValue14 = ref('')
+const outlinedValue15 = ref('')
 
 const inputRef = ref()
+const outlinedInputRef = ref()
 
 const selectAll = () => {
   inputRef.value?.select()
+}
+
+const selectAllOutlined = () => {
+  outlinedInputRef.value?.select()
 }
 
 watchLang(use)
@@ -85,7 +91,7 @@ onThemeChange()
     <var-input v-model="standardValue10" :placeholder="t('textarea')" textarea />
     <var-input v-model="standardValue11" :placeholder="t('smallSize')" size="small" />
     <var-input v-model.trim="standardValue12" :placeholder="t('trim')" />
-    <var-space>
+    <var-space align="center">
       <var-input ref="inputRef" v-model="standardValue15" :placeholder="t('selectAll')" />
       <var-button type="primary" @click="selectAll">{{ t('selectAllButton') }}</var-button>
     </var-space>
@@ -135,6 +141,10 @@ onThemeChange()
     <var-input v-model="outlinedValue10" variant="outlined" :placeholder="t('textarea')" textarea />
     <var-input v-model="outlinedValue11" variant="outlined" :placeholder="t('smallSize')" size="small" />
     <var-input v-model.trim="outlinedValue12" variant="outlined" :placeholder="t('trim')" />
+    <var-space align="center">
+      <var-input ref="outlinedInputRef" v-model="outlinedValue15" variant="outlined" :placeholder="t('selectAll')" />
+      <var-button type="primary" @click="selectAllOutlined">{{ t('selectAllButton') }}</var-button>
+    </var-space>
   </var-space>
 
   <div style="height: 40px"></div>
