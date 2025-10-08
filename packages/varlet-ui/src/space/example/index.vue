eaf58ab4b11e1c069728e28610e6a1858f86110a
haoziqaq <357229046@qq.com>
2025-10-08 11:27:34 +0800

    fix(space): fix comment vnode filter error

--- packages/varlet-ui/src/space/example/index.vue
+++ packages/varlet-ui/src/space/example/index.vue
@@ -7,51 +7,19 @@ onThemeChange()
 </script>
 
 <template>
-  <app-type>{{ t('baseUse') }}</app-type>
-  <var-space>
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-    <var-button>Button3</var-button>
-  </var-space>
-
   <app-type>{{ t('vertical') }}</app-type>
-  <var-space direction="column" size="large">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-    <var-button>Button3</var-button>
-  </var-space>
+  <var-space direction="column">
+    <div>asdadasd</div>
+    <!-- asddadada -->
+    <div v-if="false"></div>
+    <!-- 怎么会这样呢 -->
+    <div v-else>asdasdsad</div>
+    <!-- asddadada -->
+    <div>123123</div>
 
-  <app-type>{{ t('space') }}</app-type>
-  <var-space :size="[20, 20]">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-    <var-button>Button3</var-button>
-    <var-button>Button4</var-button>
-    <var-button>Button5</var-button>
-    <var-button>Button6</var-button>
-  </var-space>
-
-  <app-type>{{ t('rightAlign') }}</app-type>
-  <var-space justify="end" :size="['2.666vmin', '2.666vmin']">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-  </var-space>
-
-  <app-type>{{ t('around') }}</app-type>
-  <var-space justify="space-around" :size="['2.666vmin', '2.666vmin']">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-  </var-space>
-
-  <app-type>{{ t('center') }}</app-type>
-  <var-space justify="center" :size="['2.666vmin', '2.666vmin']">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
-  </var-space>
-
-  <app-type>{{ t('between') }}</app-type>
-  <var-space justify="space-between" :size="['2.666vmin', '2.666vmin']">
-    <var-button>Button1</var-button>
-    <var-button>Button2</var-button>
+    <!-- asddadada -->
+    <!-- asddadada -->
+    <!-- asddadada -->
+    <!-- asddadada -->
   </var-space>
 </template>
