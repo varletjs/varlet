a777e56c1664161129acd7b66fc769950baae05c
haoziqaq <357229046@qq.com>
2025-11-17 14:13:03 +0000

    Deploying to gh-page from @ varletjs/varlet@844ce2be66e608d459cefaaab32b622cf5d7b68e 🚀

---
+++ v2/assets/zh-CN-mXhcxFjn.js
@@ -0,0 +1,44 @@
+import{_ as h,b as c,e as o,m as p,p as a,q as t,w as l,P as s,Z as d}from"./index-vwO9GBzB.js";const i={components:{}},r={class:"varlet-site-doc"},j=a("h1",null,"文本省略",-1),_={class:"card"},m={id:"jie4shao4"},g=a("p",null,"主要用于省略单行文字和多行文字。",-1),u={class:"card"},x={id:"dan1xing2sheng3lüe4"},v=a("pre",{class:"hljs"},[a("code",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-ellipsis"),s(),a("span",{class:"hljs-attr"},"style"),s("="),a("span",{class:"hljs-string"},'"max-width: 170px"'),s(">")]),s(`
+    其实没有什么事情是不可能的，我都拿到世界冠军了，真的没有什么事情是不可能的。
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-ellipsis"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+`)])],-1),f={class:"card"},y={id:"duo1xing2sheng3lüe4"},b=a("pre",{class:"hljs"},[a("code",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-ellipsis"),s(),a("span",{class:"hljs-attr"},"style"),s("="),a("span",{class:"hljs-string"},'"max-width: 170px"'),s(),a("span",{class:"hljs-attr"},":line-clamp"),s("="),a("span",{class:"hljs-string"},'"3"'),s(">")]),s(`
+    其实没有什么事情是不可能的，我都拿到世界冠军了，真的没有什么事情是不可能的。
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-ellipsis"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+`)])],-1),k={class:"card"},w={id:"shuang1xiang4bang3ding4"},z=a("pre",{class:"hljs"},[a("code",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"script"),s(),a("span",{class:"hljs-attr"},"setup"),s(">")]),a("span",{class:"javascript"},[s(`
+`),a("span",{class:"hljs-keyword"},"import"),s(" { ref } "),a("span",{class:"hljs-keyword"},"from"),s(),a("span",{class:"hljs-string"},"'vue'"),s(`
+
+`),a("span",{class:"hljs-keyword"},"const"),s(" expand = ref("),a("span",{class:"hljs-literal"},"false"),s(`)
+`)]),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"script"),s(">")]),s(`
+
+`),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-space"),s(),a("span",{class:"hljs-attr"},"direction"),s("="),a("span",{class:"hljs-string"},'"column"'),s(),a("span",{class:"hljs-attr"},"size"),s("="),a("span",{class:"hljs-string"},'"large"'),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-button"),s(),a("span",{class:"hljs-attr"},"type"),s("="),a("span",{class:"hljs-string"},'"primary"'),s(" @"),a("span",{class:"hljs-attr"},"click"),s("="),a("span",{class:"hljs-string"},'"expand = !expand"'),s(">")]),s("切换"),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-button"),s(">")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-ellipsis"),s(),a("span",{class:"hljs-attr"},"style"),s("="),a("span",{class:"hljs-string"},'"max-width: 170px"'),s(),a("span",{class:"hljs-attr"},"v-model:expand"),s("="),a("span",{class:"hljs-string"},'"expand"'),s(">")]),s(`
+      其实没有什么事情是不可能的，我都拿到世界冠军了，真的没有什么事情是不可能的。
+    `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-ellipsis"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-space"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+`)])],-1),T={class:"card"},N={id:"dian3ji1wen2zi4zhan3kai1"},V=a("pre",{class:"hljs"},[a("code",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-ellipsis"),s(` 
+    `),a("span",{class:"hljs-attr"},"style"),s("="),a("span",{class:"hljs-string"},'"max-width: 170px"'),s(` 
+    `),a("span",{class:"hljs-attr"},"expand-trigger"),s("="),a("span",{class:"hljs-string"},'"click"'),s(` 
+    `),a("span",{class:"hljs-attr"},":line-clamp"),s("="),a("span",{class:"hljs-string"},'"3"'),s(` 
+    `),a("span",{class:"hljs-attr"},":tooltip"),s("="),a("span",{class:"hljs-string"},'"false"'),s(`
+  >`)]),s(`
+    其实没有什么事情是不可能的，我都拿到世界冠军了，真的没有什么事情是不可能的。
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-ellipsis"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+`)])],-1),$={class:"card"},B={id:"zi4ding4yi4To0o0lti0p"},C=a("pre",{class:"hljs"},[a("code",null,[a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-ellipsis"),s(),a("span",{class:"hljs-attr"},"style"),s("="),a("span",{class:"hljs-string"},'"max-width: 170px"'),s(),a("span",{class:"hljs-attr"},":tooltip"),s("="),a("span",{class:"hljs-string"},`"{ type: 'primary', sameWidth: false }"`),s(">")]),s(`
+    其实没有什么事情是不可能的，我都拿到世界冠军了，真的没有什么事情是不可能的。
+
+    `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"template"),s(" #"),a("span",{class:"hljs-attr"},"tooltip-content"),s(">")]),s(`
+      `),a("span",{class:"hljs-tag"},[s("<"),a("span",{class:"hljs-name"},"var-icon"),s(),a("span",{class:"hljs-attr"},"name"),s("="),a("span",{class:"hljs-string"},'"github"'),s(" />")]),s(`
+    `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+  `),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"var-ellipsis"),s(">")]),s(`
+`),a("span",{class:"hljs-tag"},[s("</"),a("span",{class:"hljs-name"},"template"),s(">")]),s(`
+`)])],-1),P=a("h2",null,"API",-1),W={class:"card"},q={id:"shu3xing4"},A=d('<table><thead><tr><th>参数</th><th>说明</th><th>类型</th><th>默认值</th></tr></thead><tbody><tr><td><code>v-model:expand</code></td><td>是否展开</td><td><em>boolean</em></td><td><code>false</code></td></tr><tr><td><code>expand-trigger</code></td><td>展开触发方式，可选值 <code>click</code></td><td><em>string</em></td><td><code>-</code></td></tr><tr><td><code>line-clamp</code></td><td>最大显示行数，兼容性见 <a href="https://caniuse.com/?search=line-clamp">line-clamp</a></td><td><em>string</em></td><td><code>-</code></td></tr><tr><td><code>tooltip</code></td><td>Tooltip 组件配置，用于深度定制。为了更方便使用，<code>sameWidth</code> 在这里默认为 <code>true</code></td><td><em>boolean | TooltipProps</em></td><td><code>true</code></td></tr></tbody></table>',1),E={class:"card"},I={id:"cha1cao2"},S=d("<table><thead><tr><th>插槽名</th><th>说明</th><th>参数</th></tr></thead><tbody><tr><td><code>default</code></td><td>文字内容</td><td><code>-</code></td></tr><tr><td><code>tooltip-content</code></td><td>Tooltip 的内容</td><td><code>-</code></td></tr></tbody></table>",1);function Z(D,F,G,H,J,K){const n=c("router-link"),e=c("var-site-code-example");return o(),p("div",r,[j,a("div",_,[a("h3",m,[t(n,{to:"#jie4shao4"},{default:l(()=>[s("#")]),_:1}),s("介绍")]),g]),a("div",u,[a("h3",x,[t(n,{to:"#dan1xing2sheng3lüe4"},{default:l(()=>[s("#")]),_:1}),s("单行省略")]),t(e,null,{default:l(()=>[v]),_:1})]),a("div",f,[a("h3",y,[t(n,{to:"#duo1xing2sheng3lüe4"},{default:l(()=>[s("#")]),_:1}),s("多行省略")]),t(e,null,{default:l(()=>[b]),_:1})]),a("div",k,[a("h3",w,[t(n,{to:"#shuang1xiang4bang3ding4"},{default:l(()=>[s("#")]),_:1}),s("双向绑定")]),t(e,null,{default:l(()=>[z]),_:1})]),a("div",T,[a("h3",N,[t(n,{to:"#dian3ji1wen2zi4zhan3kai1"},{default:l(()=>[s("#")]),_:1}),s("点击文字展开")]),t(e,null,{default:l(()=>[V]),_:1})]),a("div",$,[a("h3",B,[t(n,{to:"#zi4ding4yi4To0o0lti0p"},{default:l(()=>[s("#")]),_:1}),s("自定义 Tooltip")]),t(e,null,{default:l(()=>[C]),_:1})]),P,a("div",W,[a("h3",q,[t(n,{to:"#shu3xing4"},{default:l(()=>[s("#")]),_:1}),s("属性")]),A]),a("div",E,[a("h3",I,[t(n,{to:"#cha1cao2"},{default:l(()=>[s("#")]),_:1}),s("插槽")]),S])])}const M=h(i,[["render",Z]]);export{M as default};
